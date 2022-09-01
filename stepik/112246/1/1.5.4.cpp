f8163b7e2ec14bf5732f1ba2eae0f5855d79a20b
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-02 00:22:14 +0300

    graph/1/1.5.3

---
+++ stepik/112246/1/1.5.4.cpp
@@ -0,0 +1,34 @@
+//
+// Created by aminjon on 9/1/22.
+//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+const size_t N = 1e5 + 2;
+int C[N], n, m;
+
+auto dfs(int v, int c) -> void {
+    C[v] = c;
+
+    auto next_m = m * (v / m + 1);
+    for (int i = next_m - v; i <= n; i += m, next_m += m)
+        if (i > 0 && C[i] > c) dfs(i, c);
+}
+
+auto main() -> int32_t {
+    cin >> n >> m;
+
+    if (m <= 1) {
+        cout << n;
+        return 0;
+    }
+    fill(C, C + n + 1, N + 10);
+
+    int c = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (C[i] > c) dfs(i, c++);
+    }
+
+    cout << c << '\n';
+}
