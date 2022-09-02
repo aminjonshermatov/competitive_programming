98a0623f989f53787602d9c6183f853ed6ab0a99
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-02 21:28:21 +0300

    stepik/graph/1.8.1

--- stepik/112246/1/1.8.1.cpp
+++ stepik/112246/1/1.8.1.cpp
@@ -7,31 +7,33 @@ using namespace std;
 
 const size_t N = 1e5 + 1;
 set<int> G[N];
-int used[N], F[N];
+int used[N];
 
-int dfs(int v) {
+void dfs(int v) {
     used[v] = 1;
 
-    int cnt = 1;
     for (auto to : G[v]) {
-        if (used[to] == 0) cnt += dfs(to);
+        if (used[to] == 0) dfs(to);
     }
-
-    return F[v] = cnt;
 }
 
 auto main() -> int32_t {
     int n; cin >> n;
 
     memset(used, 0, sizeof(int) * n);
-    memset(F, 0, sizeof(int) * n);
 
     for (int i = 1; i < n; ++i) {
         int temp; cin >> temp;
         G[--temp].insert(i);
     }
 
-    dfs(0);
-    for (int i = 0; i < n; ++i) cout << F[i] << ' ';
+    //dfs(0);
+    vector<int> res;
+
+    for (int i = 1; i < n; ++i) {
+        if (G[i].empty()) res.emplace_back(i + 1);
+    }
+
+    for (auto v : res) cout << v << ' ';
     cout << '\n';
 }
