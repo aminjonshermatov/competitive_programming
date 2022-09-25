fba8f14e3c90d569ef5ef7bba8008a79b83bbc0b
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-25 17:03:24 +0300

    week_1/1729B

---
+++ codeforces/archive/block_1/week_1/1729B.cpp
@@ -0,0 +1,93 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+typedef long long ll;
+typedef long double ld;
+
+#define F first
+#define S second
+#define mk make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (ll)((x).size())
+
+#define rep(i, a, b) for (ll i = (a); (i) < (b); ++(i))
+#define forr(el, cont) for (auto &(el) : (cont))
+#define read(k) ll k; cin >> k
+
+#define IOS ios_base::sync_with_stdio(false); \
+            cin.tie(nullptr);                 \
+            cout.tie(nullptr);
+
+template<typename T>
+using V = vector<T>;
+
+template<typename T = ll>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+[[maybe_unused]] mt19937 rnd(143);
+
+const ll inf = 1e15;
+const ll MOD = 1e9 + 7;
+[[maybe_unused]] const ld pi = atan2(0, -1);
+const ld eps = 1e-6;
+
+// ========================================= PROBLEM =========================================
+
+/*
+sort(all(s));
+cout << s << '\n';
+rep(i, 0, 26) cout << setw(2) << i + 1 << " \n"[i == 25];
+rep(i, 0, 26) cout << setw(2) << (char)(i + 'a') << " \n"[i == 25];
+*/
+
+void solve() {
+    read(N);
+    string s; cin >> s;
+
+    string res;
+    for (ll i = N - 1; i >= 0; --i) {
+        if (s[i] != '0') res.pb(s[i] - '1' + 'a');
+        else {
+            int a = s[i - 2] - '0';
+            int b = s[i - 1] - '0';
+            res.pb(a * 10 + b - 1 + 'a');
+            i -= 2;
+        }
+    }
+
+    reverse(all(res));
+    cout << res;
+}
+
+bool is_multi = true;
+
+int main() {
+    // auto start = chrono::steady_clock::now();
+
+    IOS
+
+    int T = 1;
+    if (is_multi) cin >> T;
+    for (int tc = 1; tc <= T; ++tc) {
+        // cout << "Case #" << tc << ": ";
+        solve();
+        cout << '\n';
+    }
+
+    // auto finish = chrono::steady_clock::now();
+    // auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
+    // cerr << endl << "Time: " << elapsed_ms.count() << " ms\n";
+
+    return 0;
+}
