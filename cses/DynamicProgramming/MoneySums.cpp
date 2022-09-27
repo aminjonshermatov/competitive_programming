63d3db2a85c482bd8265f3f28f2c1192d5ff8350
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-27 21:50:09 +0300

    cses/MoneySums

---
+++ cses/DynamicProgramming/MoneySums.cpp
@@ -0,0 +1,87 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+typedef long long ll;
+typedef long double ld;
+
+#define F first
+#define S second
+#define mk make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (ll)((x).size())
+
+#define rep(i, a, b) for (ll i = (a); (i) < (b); ++(i))
+#define forr(el, cont) for (auto &(el) : (cont))
+#define read(k) ll k; cin >> k
+
+#define IOS ios_base::sync_with_stdio(false); \
+            cin.tie(nullptr);                 \
+            cout.tie(nullptr);
+
+template<typename T>
+using V = vector<T>;
+
+template<typename T = ll>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+[[maybe_unused]] mt19937 rnd(143);
+
+const ll inf = 1e15;
+const ll MOD = 1e9 + 7;
+[[maybe_unused]] const ld pi = atan2(0, -1);
+const ld eps = 1e-6;
+
+// ========================================= PROBLEM =========================================
+
+void solve() {
+    read(N);
+    vector<ll> pf(N + 1);
+    pf[0] = 0;
+    ll s = 0;
+    rep (i, 1, N + 1) {
+        read(a);
+        pf[i] = pf[i - 1] + a;
+        s += a;
+    }
+
+    set<ll> uniq;
+    rep (i, 1, N + 1)
+        rep (j, i, N + 1) uniq.insert(pf[j] - pf[i - 1]);
+
+    cout << uniq.size() << '\n';
+    forr(a, uniq) cout << a << ' ';
+    cout << '\n';
+}
+
+bool is_multi = false;
+
+int main() {
+    // auto start = chrono::steady_clock::now();
+
+    IOS
+
+    int T = 1;
+    if (is_multi) cin >> T;
+    for (int tc = 1; tc <= T; ++tc) {
+        // cout << "Case #" << tc << ": ";
+        solve();
+        cout << '\n';
+    }
+
+    // auto finish = chrono::steady_clock::now();
+    // auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
+    // cerr << endl << "Time: " << elapsed_ms.count() << " ms\n";
+
+    return 0;
+}
