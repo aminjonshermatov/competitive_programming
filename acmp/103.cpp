739f366ad9267379c72bec1cce71a4e0b7cf5c97
aminjonshermatov <aminjonshermatov@gmail.com>
2022-10-01 20:35:40 +0300

    acmp/102

---
+++ acmp/103.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    string  a, b;
+    cin >> a >> b;
+    if (a.size() > b.size()) swap(a, b); // b.size() - max
+
+    const int N(a.size());
+    const int M(b.size());
+
+    int rem = 0;
+    for (int i = N - 1, j = M - 1; j >= 0; --i, --j) {
+        auto num = (b[j] - '0') + rem;
+        if (i >= 0) num += (a[i] - '0');
+        b[j] = char(num % 10) + '0';
+        rem = num / 10;
+    }
+
+    if (rem != 0) b.insert(0, 1, char(rem) + '0');
+
+    cout << b  << '\n';
+    return 0;
+}
