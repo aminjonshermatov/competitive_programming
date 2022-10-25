5da543747720ebf50442d8c077aa634f6b70cb20
aminjonshermatov <aminjonshermatov@gmail.com>
2022-10-25 20:18:27 +0300

    cf_virt/494

---
+++ codeforces/archive/virtual/494/B.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+#define F first
+#define S second
+#define P pair
+#define mk make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (ll)((x).size())
+
+#define rep(i, a, b) for (ll i = (a); (i) < (b); ++(i))
+#define forr(el, cont) for (auto &(el) : (cont))
+#define read(k) ll k; cin >> k
+
+#define IOS ios_base::sync_with_stdio(false); \
+            cin.tie(nullptr);                 \
+            cout.tie(nullptr);
+
+template<typename T>
+using V = vector<T>;
+
+template<typename T = ll>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+[[maybe_unused]] mt19937 rnd(143);
+
+const ll inf = 1e15;
+const ll MOD = 1e9 + 7;
+[[maybe_unused]] const ld pi = atan2(0, -1);
+const ld eps = 1e-6;
+
+// ========================================= PROBLEM =========================================
+
+void solve() {
+    ll a, b, x;
+    cin >> a >> b >> x;
+    string aa, res;
+    rep(_, 0, (x+1)/2) aa += "01", --a, --b;
+    if ((x+1)&1) {
+        if (a > 0) {
+            res.append(a - 1, '0');
+            res += aa;
+            res.append(b, '1');
+            res.push_back('0');
+        }
+        else {
+            res.push_back('1');
+            res.append(a, '0');
+            res += aa;
+            res.append(b - 1, '1');
+        }
+    } else {
+        res.append(a, '0');
+        res += aa;
+        res.append(b, '1');
+    }
+
+    cout << res;
+}
+
+bool is_multi = false;
+
+int main() {
+    // auto start = chrono::steady_clock::now();
+
+    IOS
+
+    int T = 1;
+    if (is_multi) cin >> T;
+    for (int tc = 1; tc <= T; ++tc) {
+        // cout << "Case #" << tc << ": ";
+        solve();
+        cout << '\n';
+    }
+
+    // auto finish = chrono::steady_clock::now();
+    // auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
+    // cerr << endl << "Time: " << elapsed_ms.count() << " ms\n";
+
+    return 0;
+}
---
+++ codeforces/archive/virtual/494/D.cpp
@@ -0,0 +1,100 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+#define F first
+#define S second
+#define P pair
+#define mk make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (ll)((x).size())
+
+#define rep(i, a, b) for (ll i = (a); (i) < (b); ++(i))
+#define forr(el, cont) for (auto &(el) : (cont))
+#define read(k) ll k; cin >> k
+
+#define IOS ios_base::sync_with_stdio(false); \
+            cin.tie(nullptr);                 \
+            cout.tie(nullptr);
+
+template<typename T>
+using V = vector<T>;
+
+template<typename T = ll>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+[[maybe_unused]] mt19937 rnd(143);
+
+const ll inf = 1e15;
+const ll MOD = 1e9 + 7;
+[[maybe_unused]] const ld pi = atan2(0, -1);
+const ld eps = 1e-6;
+
+// ========================================= PROBLEM =========================================
+
+void solve() {
+    ll N, Q;
+    cin >> N >> Q;
+
+    array<ll, 31> cnt{};
+    rep(_, 0, N) {
+        read(a);
+        ++cnt[31 - __builtin_clz(a)];
+    }
+
+    V<ll> q(Q);
+    forr(a, q) cin >> a;
+
+    V<ll> res;
+    res.reserve(Q);
+    forr(a, q) {
+        ll ans = 0;
+        for (ll i = 31; i >= 0; --i) {
+            if (a >= (1ll << i)) {
+                auto c = min(cnt[i], a / (1ll << i));
+                a -= c * (1ll << i);
+                ans += c;
+            }
+            if (a <= 0) break;
+        }
+        res.pb(a == 0 ? ans : -1);
+    }
+
+    forr(a, res) cout << a << '\n';
+}
+
+bool is_multi = false;
+
+int main() {
+    // auto start = chrono::steady_clock::now();
+
+    IOS
+
+    int T = 1;
+    if (is_multi) cin >> T;
+    for (int tc = 1; tc <= T; ++tc) {
+        // cout << "Case #" << tc << ": ";
+        solve();
+        cout << '\n';
+    }
+
+    // auto finish = chrono::steady_clock::now();
+    // auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
+    // cerr << endl << "Time: " << elapsed_ms.count() << " ms\n";
+
+    return 0;
+}
