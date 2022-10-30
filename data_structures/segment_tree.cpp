f1978a3f94d1c2f0522958660113e50589681d7e
aminjonshermatov <aminjonshermatov@gmail.com>
2022-10-30 23:29:49 +0300

    ds/segtree lazy with build from array

--- data_structures/segment_tree.cpp
+++ data_structures/segment_tree.cpp
@@ -10,26 +10,29 @@ using ll = long long;
 struct segtree {
 
     static inline constexpr ll DEFAULT_VALUE = 0ll;
-    static inline constexpr ll NO_OPERATION = 0ll;
+    static inline constexpr ll NO_OPERATION = 1ll;
+    static inline constexpr ll MOD = 1e9 + 7;
 
     struct node {
-        ll val = DEFAULT_VALUE;
-        ll lazy = NO_OPERATION;
+        ll val;
+        ll lazy;
 
         void apply([[maybe_unused]] ll l, [[maybe_unused]] ll r, ll v) {
-            val += v;
-            lazy += v;
+            val *= v;
+            val %= MOD;
+            lazy *= v;
+            lazy %= MOD;
         }
 
         static node unite(const node &a, const node &b) {
             return node{
-                    min(a.val, b.val),
+                    (a.val + b.val) % MOD,
                     NO_OPERATION
             };
         }
     };
 
-    static inline constexpr node NEUTRAL_ELEMENT = {numeric_limits<ll>::max(), NO_OPERATION};
+    static inline constexpr node NEUTRAL_ELEMENT = {DEFAULT_VALUE, NO_OPERATION};
 
     ll size;
     vector<node> tree;
@@ -37,7 +40,11 @@ struct segtree {
     explicit segtree(ll n) {
         size = 1ll;
         while (size < n) size <<= 1;
-        tree.assign(2 * size - 1, {});
+        tree.assign(2 * size - 1, NEUTRAL_ELEMENT);
+    }
+
+    explicit segtree(vector<ll> &a) : segtree(ll(a.size())) {
+        build(a, 0, 0, size);
     }
 
     void print() {
@@ -59,6 +66,18 @@ struct segtree {
 
     inline void pull(ll x) {
         tree[x] = node::unite(tree[2 * x + 1], tree[2 * x + 2]);
+    }
+
+    void build(vector<ll> &a, ll x, ll lx, ll rx) {
+        if (rx - lx == 1) {
+            if (lx < ll(a.size())) tree[x] = {a[lx], NO_OPERATION};
+            return;
+        }
+
+        ll mid = lx + (rx - lx) / 2;
+        build(a, 2 * x + 1, lx, mid);
+        build(a, 2 * x + 2, mid, rx);
+        pull(x);
     }
 
     void modify(ll l, ll r, ll v, ll x, ll lx, ll rx) {
@@ -92,8 +111,7 @@ struct segtree {
     }
 
     ll get(ll l, ll r) {
-        auto res = get(l, r, 0, 0, size);
-        return res.val;
+        return get(l, r, 0, 0, size).val;
     }
 
 };
