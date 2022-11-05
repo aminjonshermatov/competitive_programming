12d045e222dad884961d788b48db3004331b3b6d
aminjonshermatov <aminjonshermatov@gmail.com>
2022-11-05 13:43:30 +0300

    stepik/1.6.1

--- stepik/199929/1/1.6.1.cpp
+++ stepik/199929/1/1.6.1.cpp
@@ -8,15 +8,21 @@ using namespace std;
 using ll = long long;
 
 auto solve() {
-    ll N, ans = 0;
-    cin >> N;
-    for (ll i = 0; i < N; ++i) {
-        ll x;
-        cin >> x;
-        ans ^= x;
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> A(N);
+    for (auto &a : A) cin >> a;
+
+    pair<ll, ll> ans = {0, 0};
+    map<ll, ll> cnt{};
+    ll cur = 0;
+    for (ll l = 0, r = 0; r < N; ++r) {
+        cur += ++cnt[A[r]] == 1;
+        while (l < r && cur > K) cur -= --cnt[A[l++]] == 0;
+        if (ans.second - ans.first <= r - l) ans = {l + 1, r + 1};
     }
 
-    cout << ans;
+    cout << ans.first << ' ' << ans.second;
 }
 
 int main() {
