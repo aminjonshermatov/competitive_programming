8c15e5210a98ff95890d5352983663a8c67b2459
aminjonshermatov <aminjonshermatov@gmail.com>
2022-11-27 13:57:54 +0300

    atcoder/abc254

---
+++ atcoder/abc/254/E.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef pair<ll, ll>        pll;
+typedef pair<int, int>      pii;
+typedef pair<ll, int>       pli;
+typedef pair<int, ll>       pil;
+typedef unsigned long long  ull;
+
+#define fi      first
+#define se      second
+#define P       pair
+#define mp      make_pair
+#define pb      push_back
+#define eb      emplace_back
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (ll)((x).size())
+
+#define rep(i, a, b)    for (auto i = (a); (i) < (b); ++(i))
+#define forr(el, cont)  for (auto &(el) : (cont))
+#define read(k)         ll k; cin >> k
+
+template<typename T = ll> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll inf = 1e15;
+const ll MOD = 1e9 + 7;
+const ld pi = atan2(0, -1);
+const ld eps = 1e-6;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> g(n + 1);
+    rep(_, 0, m) {
+        int a, b;
+        cin >> a >> b;
+        g[a].eb(b);
+        g[b].eb(a);
+    }
+
+    vector<int> dist(n + 1);
+
+    int Q;
+    cin >> Q;
+    rep(_, 0, Q) {
+        int x, k;
+        cin >> x >> k;
+
+        queue<int> q;
+        q.push(x);
+        fill(all(dist), -1);
+        dist[x] = 0;
+        int ans = 0;
+
+        while (!q.empty()) {
+            auto v = q.front(); q.pop();
+            ans += v;
+            if (dist[v] == k) continue;
+
+            for (auto u : g[v]) if (dist[u] == -1) {
+                q.push(u);
+                dist[u] = dist[v] + 1;
+            }
+        }
+        cout << ans << '\n';
+    }
+}
+
+bool is_multi = false;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T = 1;
+    if (is_multi) cin >> T;
+    for (int tc = 1; tc <= T; ++tc) solve();
+
+    return 0;
+}
