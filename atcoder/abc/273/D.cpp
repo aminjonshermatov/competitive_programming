7092c0e7158f7084ed3d5bc37aaa32df204a7acd
aminjonshermatov <aminjonshermatov@gmail.com>
2023-01-02 13:57:02 +0300

    atcoder/273

---
+++ atcoder/abc/273/D.cpp
@@ -0,0 +1,119 @@
+// #define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef pair<ll, ll>        pll;
+typedef pair<int, int>      pii;
+typedef pair<ll, int>       pli;
+typedef pair<int, ll>       pil;
+typedef unsigned long long  ull;
+
+#define fi      first
+#define se      second
+#define P       pair
+#define mp      make_pair
+#define pb      push_back
+#define eb      emplace_back
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (ll)((x).size())
+
+#define rep(i, b)       for (auto i = 0; (i) < (b); ++(i))
+#define forr(el, cont)  for (auto &(el) : (cont))
+
+template<typename T>             using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using gp_ht = gp_hash_table<K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
+
+const ll inf = 1e15;
+const ll MOD = 1e9 + 7;
+const ld pi = atan2(0, -1);
+const ld eps = 1e-6;
+
+void solve() {
+    ll H, W, rs, cs, N; cin >> H >> W >> rs >> cs >> N;
+
+    map<ll, vector<ll>> rows, cols;
+    rep(i, N) {
+        ll r, c; cin >> r >> c;
+        rows[r].eb(c);
+        cols[c].eb(r);
+    }
+    for (auto &[_, cont] : rows) sort(all(cont));
+    for (auto &[_, cont] : cols) sort(all(cont));
+
+    ll Q; cin >> Q;
+    rep(_, Q) {
+        char d;
+        ll l; cin >> d >> l;
+        switch (d) {
+            break; case 'L': {
+                auto &cur = rows[rs];
+                if (cur.empty()) {
+                    cs = max(1ll, cs - l);
+                } else {
+                    auto it = lower_bound(all(cur), cs);
+                    if (it != cur.begin()) it = prev(it);
+                    if (*it > cs) cs = max(1ll, cs - l);
+                    else cs = max(cs - l, *it + 1);
+                }
+            }
+            break; case 'R': {
+                auto &cur = rows[rs];
+                if (cur.empty()) {
+                    cs = min(W, cs + l);
+                } else {
+                    auto it = upper_bound(all(cur), cs);
+                    if (it != cur.end()) cs = min(cs + l, *it - 1);
+                    else cs = min(cs + l, W);
+                }
+            }
+            break; case 'U': {
+                auto &cur = cols[cs];
+                if (cur.empty()) {
+                    rs = max(1ll, rs - l);
+                } else {
+                    auto it = lower_bound(all(cur), rs);
+                    if (it != cur.begin()) it = prev(it);
+                    if (*it > rs) rs = max(1ll, rs - l);
+                    else rs = max(rs - l, *it + 1);
+                }
+            }
+            break; case 'D': {
+                auto &cur = cols[cs];
+                if (cur.empty()) {
+                    rs = min(H, rs + l);
+                } else {
+                    auto it = upper_bound(all(cur), rs);
+                    if (it != cur.end()) rs = min(rs + l, *it - 1);
+                    else rs = min(rs + l, H);
+                }
+            }
+        }
+        cout << rs << ' ' << cs << '\n';
+    }
+}
+
+bool is_multi = false;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T = 1;
+    if (is_multi) cin >> T;
+    for (int tc = 1; tc <= T; ++tc) solve();
+
+    return 0;
+}
