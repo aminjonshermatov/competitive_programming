b68bff7710cdb7b561e4b13491133e98b1036e86
aminjonshermatov <aminjonshermatov@gmail.com>
2023-01-03 20:02:22 +0300

    cf/hello_2023

---
+++ codeforces/special/hello_2023/A.cpp
@@ -0,0 +1,74 @@
+// #define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef pair<ll, ll>        pll;
+typedef pair<int, int>      pii;
+typedef pair<ll, int>       pli;
+typedef pair<int, ll>       pil;
+typedef unsigned long long  ull;
+
+#define fi      first
+#define se      second
+#define P       pair
+#define mp      make_pair
+#define pb      push_back
+#define eb      emplace_back
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (ll)((x).size())
+
+#define rep(i, b)       for (auto i = 0; (i) < (b); ++(i))
+#define forr(el, cont)  for (auto &(el) : (cont))
+
+template<typename T>             using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using gp_ht = gp_hash_table<K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
+
+const ll inf = 1e15;
+const ll MOD = 1e9 + 7;
+const ld pi = atan2(0, -1);
+const ld eps = 1e-6;
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    for (int i = 0; i < n - 1; ++i) {
+        if (s[i] == 'R' && s[i  +1] == 'L') {
+            cout << 0 << '\n';
+            return;
+        }
+        if (s[i] == 'L' && s[i + 1] == 'R') {
+            cout << i + 1 << '\n';
+            return;
+        }
+    }
+
+    cout << -1 << '\n';
+}
+
+bool is_multi = true;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T = 1;
+    if (is_multi) cin >> T;
+    for (int tc = 1; tc <= T; ++tc) solve();
+
+    return 0;
+}
