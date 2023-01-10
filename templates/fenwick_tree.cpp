f552deb6f960821314ec3523602e1e7ab94da270
aminjonshermatov <aminjonshermatov@gmail.com>
2023-01-11 00:00:50 +0300

    templates/fenwick_tree

--- templates/fenwick_tree.cpp
+++ templates/fenwick_tree.cpp
@@ -5,11 +5,15 @@
 
 using namespace std;
 
-template <typename T> struct fenwick_tree {
+template <typename T = int> struct fenwick_tree {
     int n;
     vector<T> bit;
 
     explicit fenwick_tree(int n_) : n(n_), bit(n_, T(0))  { }
+
+    template<typename U = T> explicit fenwick_tree(const vector<U> &A) : n(A.size()), bit(A.size(), T(0)) {
+        for (auto idx = 0u; idx < n; ++idx) modify(idx, A[idx]);
+    }
 
     auto modify(int idx, T val) -> void {
         for (; idx < n; idx = idx | (idx + 1)) bit[idx] += val;
@@ -27,11 +31,15 @@ template <typename T> struct fenwick_tree {
 
 };
 
-template <typename T> struct fenwick_tree_range_update_point_query {
+template <typename T = int> struct fenwick_tree_range_update_point_query {
     int n;
     vector<T> bit;
 
     explicit fenwick_tree_range_update_point_query(int n_) : n(n_), bit(n_, T(0))  { }
+
+    template<typename U = T> explicit fenwick_tree_range_update_point_query(const vector<U> &A) : n(A.size()) , bit(A.size(), T(0)) {
+        for (auto idx = 0u; idx < n; ++idx) add(idx, A[idx]);
+    }
 
     auto add(int idx, T val) -> void {
         for (; idx < n; idx = idx | (idx + 1)) bit[idx] += val;
