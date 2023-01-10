f20a50bde7875bf6f56bfc5f0d5e49f51e026048
aminjonshermatov <aminjonshermatov@gmail.com>
2023-01-10 23:19:58 +0300

    templates/primes

--- templates/primes.cpp
+++ templates/primes.cpp
@@ -5,21 +5,21 @@
 
 using namespace std;
 
-struct primes {
-    ll n;
-    vector<ll> lp, pr;
+template<typename T> struct primes {
+    T n;
+    vector<T> lp, pr;
 
-    explicit primes(ll n_) : n(n_) { lp.assign(n + 1, 0); }
+    explicit primes(T n_) : n(n_) { lp.assign(n + 1, T(0)); find_primes(); }
 
-    auto find_primes() {
-        for (ll i = 2; i <= n; ++i) {
+    auto find_primes() -> void {
+        for (T i = 2; i <= n; ++i) {
             if (lp[i] == 0) {
                 lp[i] = i;
                 pr.push_back(i);
             }
-            for (ll j = 0; i * pr[j] <= n; ++j) {
+            for (T j = 0; i * pr[j] <= n; ++j) {
                 lp[i * pr[j]] = pr[j];
-                if (pr[j] == lp[i]) { break; }
+                if (pr[j] == lp[i]) break;
             }
         }
     }
