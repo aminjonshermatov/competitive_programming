1dc7f2781b09d2d62da5e537917342330a433435
aminjonshermatov <aminjonshermatov@gmail.com>
2023-01-10 23:51:56 +0300

    templates/dsu

--- temp.cpp
+++ temp.cpp
@@ -40,6 +40,42 @@ const ll MOD = 1e9 + 7;
 const ld pi = atan2(0, -1);
 const ld eps = 1e-6;
 
+class Dsu {
+    int n;
+    vector<int> parent, rank;
+public:
+    explicit Dsu(int n_) : n(n_) {
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        rank.assign(n, 1);
+    }
+
+    auto find(int v) -> int {
+        assert(0 <= v && v < n);
+        if (parent[v] != v) parent[v] = find(parent[v]);
+        return parent[v];
+    }
+
+    auto merge(int v, int u) -> int {
+        assert(0 <= v && v < n);
+        assert(0 <= u && u < n);
+        auto pv = find(v);
+        auto pu = find(u);
+        if (pv == pu) return pv;
+
+        if (rank[pv] < rank[pu]) swap(pv, pu);
+        parent[pu] = pv;
+        if (rank[pv] == rank[pu]) ++rank[pv];
+        return pv;
+    }
+
+    auto is_same(int v, int u) -> bool {
+        assert(0 <= v && v < n);
+        assert(0 <= u && u < n);
+        return find(v) == find(u);
+    }
+};
+
 void solve() {
 
 }
--- templates/dsu.cpp
+++ templates/dsu.cpp
@@ -3,35 +3,40 @@
 //
 #include <bits/stdc++.h>
 
-#define all(x) begin(x), end(x)
-
 using namespace std;
 
-class DSU {
+class Dsu {
+    int n;
     vector<int> parent, rank;
 public:
-    explicit DSU(int n) {
+    explicit Dsu(int n_) : n(n_) {
         parent.resize(n);
-        iota(all(parent), 0);
+        iota(parent.begin(), parent.end(), 0);
         rank.assign(n, 1);
     }
 
     auto find(int v) -> int {
+        assert(0 <= v && v < n);
         if (parent[v] != v) parent[v] = find(parent[v]);
         return parent[v];
     }
 
-    auto merge(int v, int u) -> void {
+    auto merge(int v, int u) -> int {
+        assert(0 <= v && v < n);
+        assert(0 <= u && u < n);
         auto pv = find(v);
         auto pu = find(u);
-        if (pv == pu) return;
+        if (pv == pu) return pv;
 
         if (rank[pv] < rank[pu]) swap(pv, pu);
         parent[pu] = pv;
         if (rank[pv] == rank[pu]) ++rank[pv];
+        return pv;
     }
 
     auto is_same(int v, int u) -> bool {
+        assert(0 <= v && v < n);
+        assert(0 <= u && u < n);
         return find(v) == find(u);
     }
 };
