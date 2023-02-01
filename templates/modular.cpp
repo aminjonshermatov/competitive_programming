6c647071c69ecc5dd46ec152355803a26ed38638
aminjonshermatov <aminjonshermatov@gmail.com>
2023-02-01 12:59:58 +0300

    templates/modular

--- templates/modular.cpp
+++ templates/modular.cpp
@@ -5,40 +5,34 @@
 
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
+const ::int64_t MOD = 1e9 + 7;
 
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-ll rnd(ll B) {
-    return (ull)rng() % B;
-}
+template<::int64_t mod = MOD> struct mint {
+    ::int64_t x;
 
-const uint MOD = 998244353;
-template<uint mod = MOD> struct mint { // 1000000007  1000000009
-    uint x;
-
-    mint() : x(0) {}
-    mint(ll _x) {
+    mint() : x(0) { }
+    explicit mint(::int64_t _x) {
         _x %= mod;
         if (_x < 0) _x += mod;
         x = _x;
     }
 
-    mint& operator += (const mint &a) {
-        x += a.x;
+    mint& operator += (const mint &other) {
+        x += other.x;
         if (x >= mod) x -= mod;
         return *this;
     }
-    mint& operator -= (const mint &a) {
-        x += mod - a.x;
+
+    mint& operator -= (const mint &other) {
+        x += mod - other.x;
         if (x >= mod) x -= mod;
         return *this;
     }
-    mint& operator *= (const mint &a) {
-        x = (ull)x * a.x % mod;
+    mint& operator *= (const mint &other) {
+        x = x * other.x % mod;
         return *this;
     }
-    mint pow(ll pw) const {
+    mint pow(::int64_t pw) const {
         mint res = 1;
         mint cur = *this;
         while(pw) {
@@ -50,151 +44,69 @@ template<uint mod = MOD> struct mint { // 1000000007  1000000009
     }
     mint inv() const {
         assert(x != 0);
-        uint t = x;
-        uint res = 1;
+        ::int64_t t = x;
+        ::int64_t res = 1;
         while(t != 1) {
-            uint z = mod / t;
-            res = (ull)res * (mod - z) % mod;
+            ::int64_t z = mod / t;
+            res = res * (mod - z) % mod;
             t = mod - t * z;
         }
         return res;
     }
-    mint& operator /= (const mint &a) {
-        return *this *= a.inv();
-    }
-    mint operator + (const mint &a) const {
-        return mint(*this) += a;
-    }
-    mint operator - (const mint &a) const {
-        return mint(*this) -= a;
-    }
-    mint operator * (const mint &a) const {
-        return mint(*this) *= a;
-    }
-    mint operator / (const mint &a) const {
-        return mint(*this) /= a;
-    }
+    mint& operator /= (const mint &other) { return *this *= other.inv(); }
 
-    bool sqrt(mint &res) const {
-        if (mod == 2 || x == 0) {
-            res = *this;
-            return true;
-        }
-        if (pow((mod - 1) / 2) != 1) return false;
-        if (mod % 4 == 3) {
-            res = pow((mod + 1) / 4);
-            return true;
-        }
-        int pw = (mod - 1) / 2;
-        int K = 30;
-        while((1 << K) > pw) K--;
-        while(true) {
-            mint t = rnd(mod);
-            mint a = 0, b = 0, c = 1;
-            for (int k = K; k >= 0; k--) {
-                a = b * b;
-                b = b * c * 2;
-                c = c * c + a * *this;
-                if (((pw >> k) & 1) == 0) continue;
-                a = b;
-                b = b * t + c;
-                c = c * t + a * *this;
-            }
-            if (b == 0) continue;
-            c -= 1;
-            c *= mint() - b.inv();
-            if (c * c == *this) {
-                res = c;
-                return true;
-            }
-        }
-        assert(false);
-    }
+    mint operator + (const mint &other) const { return mint(*this) += other; }
+    mint operator - (const mint &other) const { return mint(*this) -= other; }
+    mint operator * (const mint &other) const { return mint(*this) *= other; }
+    mint operator / (const mint &other) const { return mint(*this) /= other; }
 
-    bool operator == (const mint &a) const {
-        return x == a.x;
-    }
-    bool operator != (const mint &a) const {
-        return x != a.x;
-    }
-    bool operator < (const mint &a) const {
-        return x < a.x;
-    }
+    bool operator == (const mint &other) const { return x == other.x; }
+    bool operator != (const mint &other) const { return x != other.x; }
+    bool operator <  (const mint &other) const { return x < other.x; }
 };
-template<uint mod = MOD> struct Factorials {
-    using Mint = mint<mod>;
-    vector<Mint> f, fi;
 
-    Factorials() : f(), fi() {}
-    Factorials(int n) {
+template<::int64_t mod = MOD> struct Factorials {
+    using Mint = mint<mod>;
+    vector<Mint> fact, tcaf;
+
+    Factorials() : fact(), tcaf() { }
+    explicit Factorials(int n) {
         n += 10;
-        f = vector<Mint>(n);
-        fi = vector<Mint>(n);
-        f[0] = 1;
-        for (int i = 1; i < n; i++)
-            f[i] = f[i - 1] * i;
-        fi[n - 1] = f[n - 1].inv();
-        for (int i = n - 1; i > 0; i--)
-            fi[i - 1] = fi[i] * i;
+        fact.assign(n);
+        tcaf.assign(n);
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) fact[i] = fact[i - 1] * i;
+        tcaf[n - 1] = fact[n - 1].inv();
+        for (int i = n - 1; i > 0; i--) tcaf[i - 1] = tcaf[i] * i;
     }
 
     Mint C(int n, int k) {
         if (k < 0 || k > n) return 0;
-        return f[n] * fi[k] * fi[n - k];
+        return fact[n] * tcaf[k] * tcaf[n - k];
     }
 };
-template<uint mod = MOD> struct Powers {
+template<::int64_t mod = MOD> struct Powers {
     using Mint = mint<mod>;
-    vector<Mint> p, pi;
+    vector<Mint> pw, wp;
 
-    Powers() : p(), pi() {}
+    Powers() : pw(), wp() { }
     Powers(int n, Mint x) {
         n += 10;
         if (x == 0) {
-            p = vector<Mint>(n);
-            p[0] = 1;
+            pw.resize(n);
+            pw[0] = 1;
         } else {
-            p = vector<Mint>(n);
-            pi = vector<Mint>(n);
-            p[0] = pi[0] = 1;
+            pw.resize(n);
+            wp.resize(n);
+            pw[0] = wp[0] = 1;
             Mint xi = x.inv();
             for (int i = 1; i < n; i++) {
-                p[i] = p[i - 1] * x;
-                pi[i] = pi[i - 1] * xi;
+                pw[i] = pw[i - 1] * x;
+                wp[i] = wp[i - 1] * xi;
             }
         }
     }
 
-    Mint pow(int n) {
-        if (n >= 0)
-            return p[n];
-        else
-            return pi[-n];
-    }
-};
-template<uint mod = MOD> struct Inverses {
-    using Mint = mint<mod>;
-    vector<Mint> ii;
-
-    Inverses() : ii() {}
-    Inverses(int n) {
-        n += 10;
-        ii = vector<Mint>(n);
-        ii[1] = 1;
-        for (int x = 2; x < n; x++)
-            ii[x] = Mint() - ii[mod % x] * (mod / x);
-    }
-
-    Mint inv(Mint x) {
-        assert(x != 0);
-        uint t = x.x;
-        uint res = 1;
-        while(t >= (int)ii.size()) {
-            uint z = mod / t;
-            res = (ull)res * (mod - z) % mod;
-            t = mod - t * z;
-        }
-        return ii[t] * res;
-    }
+    Mint pow(int n) { return (n >= 0 ? pw : wp)[abs(n)]; }
 };
 using Mint = mint<>;
