e1bdecc1dfd4a6d54b7d8c8403c4d1f8c3660a9d
aminjonshermatov <aminjonshermatov@gmail.com>
2023-03-12 13:07:26 +0300

    templates/segtree | has_lazy: Bool

--- templates/segment_tree.cpp
+++ templates/segment_tree.cpp
@@ -12,50 +12,55 @@ auto L =    []<typename T>(const T &tree_val, const T &given) -> T { return tree
 
 template<typename T = int> struct segment_tree {
 
-    static inline constexpr T DEFAULT_VALUE = T(numeric_limits<T>::min());
-    static inline constexpr T NO_OPERATION = T(-1);
+    static inline constexpr T DEFAULT_VALUE = T(0);
+    static inline constexpr T DEFAULT_LAZY = T(0);
 
     template<typename U> struct node {
         U val;
         U lazy;
         int right;
+        bool has_lazy;
 
         void apply([[maybe_unused]] int l, [[maybe_unused]] int r, U v) {
-            val = v;
-            lazy = v;
+            val += v;
+            lazy += v;
+            has_lazy = true;
         }
 
         template<typename V> static node unite(const node<V> &a, const node<V> &b) {
             return node{
                     max(a.val, b.val),
-                    NO_OPERATION,
-                    b.right
+                    DEFAULT_LAZY,
+                    b.right,
+                    false
             };
         }
     };
 
-    static inline constexpr node<T> NEUTRAL_ELEMENT = {DEFAULT_VALUE, NO_OPERATION};
+    static inline constexpr node<T> NEUTRAL_ELEMENT = {DEFAULT_VALUE, DEFAULT_LAZY, -1, false};
 
     int size;
     vector<node<T>> tree;
-
+private:
     explicit segment_tree(int n) {
         size = 1;
         while (size < n) size <<= 1;
         tree.assign(2 * size - 1, NEUTRAL_ELEMENT);
     }
+public:
 
     template<typename U = T> explicit segment_tree(vector<U> &A) : segment_tree(A.size()) {
         build(A, 0, 0, size);
     }
 
     inline void push(int x, int lx, int rx) {
-        if (rx - lx == 1 || tree[x].lazy == NO_OPERATION) return;
+        if (rx - lx == 1 || !tree[x].has_lazy) return;
 
         int mid = lx + (rx - lx) / 2;
         tree[2 * x + 1].apply(lx, mid, tree[x].lazy);
         tree[2 * x + 2].apply(mid, rx, tree[x].lazy);
-        tree[x].lazy = NO_OPERATION;
+        tree[x].lazy = DEFAULT_LAZY;
+        tree[x].has_lazy = false;
     }
 
     inline void pull(int x) {
@@ -65,7 +70,7 @@ template<typename T = int> struct segment_tree {
     template<typename U = T>
     void build(vector<U> &A, int x, int lx, int rx) {
         if (rx - lx == 1) {
-            if (lx < A.size()) tree[x] = {A[lx], NO_OPERATION, lx};
+            if (lx < A.size()) tree[x] = {A[lx], DEFAULT_LAZY, lx, false};
             return;
         }
 
@@ -105,9 +110,8 @@ template<typename T = int> struct segment_tree {
         return res;
     }
 
-    T get(int l, int r) {
-        return get(l, r, 0, 0, size).val;
-    }
+    T get(int l, int r) { return get(l, r, 0, 0, size).val; }
+    T get(int idx) { return get(idx, idx + 1, 0, 0, size).val; }
 
     template<typename Op> T get_first(T v, int i, int x, int lx, int rx, Op &op) {
         if (!op(tree[x].val, v) || tree[x].right < i) return -1;
