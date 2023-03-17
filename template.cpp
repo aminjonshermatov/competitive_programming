fa64cecf4285b77577398c6a7a96ca714153a04e
aminjonshermatov <aminjonshermatov@gmail.com>
2023-03-17 20:43:05 +0300

    atcoder/dp/H

--- template.cpp
+++ template.cpp
@@ -17,31 +17,33 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-typedef long long           ll;
-typedef long double         ld;
-typedef pair<ll, ll>        pll;
-typedef pair<int, int>      pii;
-typedef pair<ll, int>       pli;
-typedef pair<int, ll>       pil;
-typedef unsigned long long  ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef unsigned long long ull;
 
-#define fi      first
-#define se      second
-#define P       pair
-#define mp      make_pair
-#define pb      push_back
-#define eb      emplace_back
-#define all(x)  (x).begin(), (x).end()
+#define fi first
+#define se second
+#define P pair
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define sz(x)   (ll)(x.size())
+#define sz(x) (ll)(x.size())
 
-#define rep(i, b)       for (auto i = 0; i < b; ++i)
-#define forr(el, cont)  for (auto &el : cont)
+#define rep(i, b) for (auto i = 0; i < b; ++i)
+#define forr(el, cont) for (auto &el: cont)
 
-template<typename T>             using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<typename K, typename V> using gp_ht = gp_hash_table<K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V>
+using gp_ht = gp_hash_table<K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
 
-inline constexpr auto  inf = numeric_limits<int>::max();
+inline constexpr auto inf = numeric_limits<int>::max();
 inline constexpr auto ninf = numeric_limits<int>::min();
 inline constexpr int MOD = 1e9 + 7;
 inline const ld pi = ::atan2(0, -1);
@@ -50,26 +52,26 @@ inline constexpr ld eps = 1e-6;
 mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
 
 void solve() {
-
 }
 
 //#define MEASURE_TIME
 bool is_multi = false;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 #ifdef MEASURE_TIME
-    auto start = chrono::steady_clock::now();
+  auto start = chrono::steady_clock::now();
 #endif
-    int T = 1;
-    if (is_multi) cin >> T;
-    for (int tc = 1; tc <= T; ++tc) solve();
+  int T = 1;
+  if (is_multi) cin >> T;
+  for (int tc = 1; tc <= T; ++tc) solve();
 #ifdef MEASURE_TIME
-    auto finish = chrono::steady_clock::now();
-    auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
-    cerr << endl << "Time: " << elapsed_ms.count() << " ms\n";
+  auto finish = chrono::steady_clock::now();
+  auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
+  cerr << endl
+       << "Time: " << elapsed_ms.count() << " ms\n";
 #endif
-    return 0;
+  return 0;
 }
