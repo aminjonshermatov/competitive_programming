d181c9af01a6c4cc7e96d3be112577b5be189847
aminjonshermatov <aminjonshermatov@gmail.com>
2023-03-17 22:10:15 +0300

    atcoder_abc/057

---
+++ atcoder/abc/057/D.cpp
@@ -0,0 +1,122 @@
+// #define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...) 42
+#endif
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef unsigned long long ull;
+
+#define fi first
+#define se second
+#define P pair
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (ll)(x.size())
+
+#define rep(i, b) for (auto i = 0; i < b; ++i)
+#define forr(el, cont) for (auto &el: cont)
+
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V>
+using gp_ht = gp_hash_table<K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
+
+inline constexpr auto inf = numeric_limits<int>::max();
+inline constexpr auto ninf = numeric_limits<int>::min();
+inline constexpr int MOD = 1e9 + 7;
+inline const ld pi = ::atan2(0, -1);
+inline constexpr ld eps = 1e-6;
+
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MX = 51;
+ll C[MX][MX];
+
+void solve() {
+  int n, a, b; cin >> n >> a >> b;
+  vector<ll> V(n);
+  forr(v, V) cin >> v;
+
+  sort(rall(V));
+  vector<ll> pf(n);
+  pf[0] = V[0];
+  for (int i = 1; i < n; ++i) {
+    pf[i] = pf[i - 1] + V[i];
+  }
+  int mi = 50;
+  ll ms = 0;
+  rep(i, n) {
+    if ((i + 1) < a || (i + 1) > b) continue ;
+    if (ms * (i + 1) < pf[i] * (mi + 1)) {
+      mi = i;
+      ms = pf[i];
+    }
+  }
+
+  ll cnt = 0;
+  rep(i, n) {
+    if ((i + 1) < a || (i + 1) > b) continue ;
+    if (ms * (i + 1) != pf[i] * (mi + 1)) continue ;
+    int x = 0, y = 0;
+    rep(j, n) {
+      if (V[j] == V[i]) {
+        ++x;
+        if (j <= i) ++y;
+      }
+    }
+    cnt += C[x][y];
+  }
+  cout << fixed << setprecision(15) << ld(ms) / (mi + 1) << '\n';
+  cout << cnt << '\n';
+}
+
+//#define MEASURE_TIME
+bool is_multi = false;
+
+int main() {
+  for (int i = 1; i < MX; ++i) {
+    C[i][0] = C[i][i] = 1;
+    for (int j = 1; j < i; ++j) {
+      C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+    }
+  }
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+#ifdef MEASURE_TIME
+  auto start = chrono::steady_clock::now();
+#endif
+  int T = 1;
+  if (is_multi) cin >> T;
+  for (int tc = 1; tc <= T; ++tc) solve();
+#ifdef MEASURE_TIME
+  auto finish = chrono::steady_clock::now();
+  auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
+  cerr << endl
+       << "Time: " << elapsed_ms.count() << " ms\n";
+#endif
+  return 0;
+}
