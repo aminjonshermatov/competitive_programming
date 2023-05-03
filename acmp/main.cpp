a6e1ffb4f650b98159dc32cb28f054ca64ee5693
aminjonshermatov <aminjonshermatov@gmail.com>
2023-05-03 10:44:03 +0300

    acmp/0194

--- acmp/main.cpp
+++ acmp/main.cpp
@@ -9,7 +9,37 @@
 
 using namespace std;
 
+using u64 = ::uint64_t;
+
+u64 solve(int n) {
+  u64 all_set = (1ull << (n + 1)) - 1;
+  int offset = 2;
+  auto dp = [&](auto &f, int last, u64 used) -> u64 {
+    if ((used & all_set) == all_set) return 1;
+    u64 loc = 0;
+    for (int nw = max(0, last - offset); nw <= min(n, last + offset); ++nw) {
+      if ((used >> nw) & 1) continue;
+      loc += f(f, nw, used | (1ull << nw));
+    }
+    return loc;
+  };
+  return dp(dp, 1, 0b11);
+}
+
 void solve() {
+  /*
+  // 1 1 2 4 6 9 14 21 31 46
+  for (int n = 1; n <= 10; ++n) {
+    cout << solve(n) << " \n"[n == 10];
+  }*/
+  int n; cin >> n;
+  vector<u64> dp(max(3, n));
+  dp[0] = dp[1] = 1;
+  dp[2] = 2;
+  for (int i = 3; i < n; ++i) {
+    dp[i] = dp[i - 1] + dp[i - 3] + 1;
+  }
+  cout << dp[n - 1] << '\n';
 }
 
 bool is_multi = false;
