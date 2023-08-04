94f9c89fdc56b696e6e915675eecc0fda6f64a4a
aminjonshermatov <aminjonshermatov@gmail.com>
2023-08-04 22:30:53 +0300

    cses/strings/MinimalRotation{nlogn_TLE}

--- cses/StringAlgorithms/MinimalRotation{nlogn_TLE}.cpp
+++ cses/StringAlgorithms/MinimalRotation{nlogn_TLE}.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 
 #ifdef LOCAL
@@ -9,13 +8,13 @@
 
 using namespace std;
 
-template <char MIN_ALPHA = 'a', char MAX_ALPHA = 'z'>
+template <size_t ALPHA = 26, char MIN_ALPHA = 'a'>
 vector<int> suffix_array(string_view s) {
-  assert(all_of(s.begin(), s.end(), [](char ch) { return clamp(ch, MIN_ALPHA, MAX_ALPHA) == ch; }));
+  assert(all_of(s.begin(), s.end(), [](char ch) { return clamp<char>(ch, MIN_ALPHA, MIN_ALPHA + ALPHA - 1) == ch; }));
 
   const auto n = static_cast<int>(s.size());
-  const auto nn = max(MAX_ALPHA - MIN_ALPHA + 1, n) + 1;
-  vector<int> sa(n), c(n), cnt(nn, 0);
+  const auto nn = max<int>(ALPHA, n) + 1;
+  vector<int> p(n), c(n), cnt(nn, 0);
   for (auto ch : s) {
     ++cnt[ch - MIN_ALPHA];
   }
@@ -23,38 +22,38 @@ vector<int> suffix_array(string_view s) {
     cnt[i] += cnt[i - 1];
   }
   for (int i = 0; i < n; ++i) {
-    sa[--cnt[s[i] - MIN_ALPHA]] = i;
+    p[--cnt[s[i] - MIN_ALPHA]] = i;
   }
-  c[sa[0]] = 0;
+  c[p[0]] = 0;
   for (int i = 1; i < n; ++i) {
-    c[sa[i]] = c[sa[i - 1]] + (s[sa[i - 1]] != s[sa[i]]);
+    c[p[i]] = c[p[i - 1]] + (s[p[i - 1]] != s[p[i]]);
   }
 
-  vector<int> nsa(n), nc(n);
+  vector<int> np(n), nc(n);
   for (int k = 1; k < n; k <<= 1) {
     for (int i = 0; i < n; ++i) {
-      nsa[i] = (sa[i] + n - k) % n;
+      np[i] = (p[i] + n - k) % n;
     }
     fill(cnt.begin(), cnt.end(), 0);
     for (int i = 0; i < n; ++i) {
-      ++cnt[c[nsa[i]]];
+      ++cnt[c[np[i]]];
     }
     for (int i = 1; i < nn; ++i) {
       cnt[i] += cnt[i - 1];
     }
     for (int i = n - 1; i >= 0; --i) {
-      sa[--cnt[c[nsa[i]]]] = nsa[i];
+      p[--cnt[c[np[i]]]] = np[i];
     }
-    nc[sa[0]] = 0;
+    nc[p[0]] = 0;
     for (int i = 1; i < n; ++i) {
-      auto cur = pair(c[sa[i]], c[(sa[i] + k) % n]);
-      auto prv = pair(c[sa[i - 1]], c[(sa[i - 1] + k) % n]);
-      nc[sa[i]] = nc[sa[i - 1]] + (cur != prv);
+      auto cur = pair(c[p[i]], c[(p[i] + k) % n]);
+      auto prv = pair(c[p[i - 1]], c[(p[i - 1] + k) % n]);
+      nc[p[i]] = nc[p[i - 1]] + (cur != prv);
     }
     nc.swap(c);
   }
 
-  return sa;
+  return p;
 }
 
 void solve() {
@@ -63,22 +62,10 @@ void solve() {
 
   const auto n = int(s.size());
   auto sa = suffix_array(s);
-  dbg(sa);
-  int k = sa[0];
-  vector<bool> seen(n, false);
-  seen[k] = true;
-  for (;;) {
-    auto nk = (k + n - 1) % n;
-    if (seen[nk] || s[sa[0]] != s[nk]) {
-      break;
-    }
-    seen[nk] = true;
-    k = nk;
-  }
-  for (int i = k; i < n; ++i) {
+  for (int i = sa[0]; i < n; ++i) {
     cout << s[i];
   }
-  for (int i = 0; i < k; ++i) {
+  for (int i = 0; i < sa[0]; ++i) {
     cout << s[i];
   }
   cout << '\n';
