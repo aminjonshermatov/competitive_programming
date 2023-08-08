401322f793d096fcfd8b7d35f988c5c16571255d
aminjonshermatov <aminjonshermatov@gmail.com>
2023-08-08 21:17:45 +0300

    cses/strings/SubstringOrderI

---
+++ cses/StringAlgorithms/SubstringOrderI.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...) 42
+#endif
+
+using namespace std;
+
+template <size_t ALPHA = 27, char MIN_ALPHA = 'a' - 1>
+vector<int> suffix_array(string_view s) {
+  assert(all_of(s.begin(), s.end(), [](char ch) { return clamp<char>(ch, MIN_ALPHA, MIN_ALPHA + ALPHA - 1) == ch; }));
+  assert(s.back() == MIN_ALPHA);
+
+  const auto n = static_cast<int>(s.size());
+  const auto nn = max<int>(ALPHA, n) + 1;
+  vector<int> p(n), c(n), cnt(nn, 0);
+  for (auto ch : s) {
+    ++cnt[ch - MIN_ALPHA];
+  }
+  for (int i = 1; i < nn; ++i) {
+    cnt[i] += cnt[i - 1];
+  }
+  for (int i = 0; i < n; ++i) {
+    p[--cnt[s[i] - MIN_ALPHA]] = i;
+  }
+  c[p[0]] = 0;
+  for (int i = 1; i < n; ++i) {
+    c[p[i]] = c[p[i - 1]] + (s[p[i - 1]] != s[p[i]]);
+  }
+
+  vector<int> np(n), nc(n);
+  for (int k = 1; k < n; k <<= 1) {
+    for (int i = 0; i < n; ++i) {
+      np[i] = (p[i] + n - k) % n;
+    }
+    fill(cnt.begin(), cnt.end(), 0);
+    for (int i = 0; i < n; ++i) {
+      ++cnt[c[np[i]]];
+    }
+    for (int i = 1; i < nn; ++i) {
+      cnt[i] += cnt[i - 1];
+    }
+    for (int i = n - 1; i >= 0; --i) {
+      p[--cnt[c[np[i]]]] = np[i];
+    }
+    nc[p[0]] = 0;
+    for (int i = 1; i < n; ++i) {
+      auto cur = pair(c[p[i]], c[(p[i] + k) % n]);
+      auto prv = pair(c[p[i - 1]], c[(p[i - 1] + k) % n]);
+      nc[p[i]] = nc[p[i - 1]] + (cur != prv);
+    }
+    nc.swap(c);
+  }
+
+  return p;
+}
+
+template <size_t ALPHA = 27, char MIN_ALPHA = 'a' - 1>
+pair<vector<int>, vector<int>> lcp_array(string_view s) {
+  assert(s.back() == MIN_ALPHA);
+
+  const auto n = static_cast<int>(s.size());
+  const auto sa = suffix_array<ALPHA, MIN_ALPHA>(s);
+  vector<int> pos(n);
+  for (int i = 0; i < n; ++i) {
+    pos[sa[i]] = i;
+  }
+  vector<int> lcp(n, 0); // lcp(s[sa[i]], s[sa[i + 1]])
+  int k = 0;
+  for (int i = 0; i < n; ++i) {
+    k = max(k - 1, 0);
+    if (pos[i] == n - 1) continue;
+    auto j = sa[pos[i] + 1];
+    while (s[i + k] == s[j + k]) ++k;
+    lcp[pos[i]] = k;
+  }
+
+  return {lcp, sa};
+}
+
+using i64 = int64_t;
+
+void solve() {
+  string t; cin >> t;
+  i64 k; cin >> k;
+
+  t.push_back('a' - 1);
+  const auto [lcp, sa] = lcp_array(t);
+  dbg(sa);
+  dbg(lcp);
+  const auto n = int(t.size());
+  for (int i = 0; i < n; ++i) {
+    // n - sa[i] - lcp[i]
+    auto cur = n - sa[i] - (i > 0 ? lcp[i - 1] : 0) - 1;
+    if (cur < k) {
+      k -= cur;
+    } else {
+      for (int j = 0; j < (i > 0 ? lcp[i - 1] : 0) + k; ++j) {
+        cout << t[sa[i] + j];
+      }
+      cout << '\n';
+      return;
+    }
+  }
+  assert(false);
+}
+
+bool is_multi = false;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int T = 1;
+  if (is_multi) cin >> T;
+  for (int tc = 1; tc <= T; ++tc) solve();
+  return 0;
+}
