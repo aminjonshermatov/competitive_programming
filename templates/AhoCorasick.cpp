8f8e4d9d5f176fe4045370bf8e1d5d4482cd2649
aminjonshermatov <aminjonshermatov@gmail.com>
2023-08-29 23:51:25 +0300

    templates/AhoCorasick added term-suf-links

--- templates/AhoCorasick.cpp
+++ templates/AhoCorasick.cpp
@@ -8,10 +8,10 @@ using namespace std;
 template <size_t ALPHA = 26, char MIN_ALPHA = 'a'>
 struct AhoCorasick {
   struct Node {
-    int link;
+    int suf_link, term_suf_link;
     vector<int> ids;
     array<int, ALPHA> next{}, go{};
-    Node() : link(-1) {
+    Node() : suf_link(-1), term_suf_link(-1) {
       next.fill(-1);
       go.fill(-1);
     }
@@ -38,7 +38,8 @@ struct AhoCorasick {
   }
 
   void build() {
-    t[ROOT].link = ROOT;
+    t[ROOT].suf_link = ROOT;
+    t[ROOT].term_suf_link = ROOT;
     for (int ch = 0; ch < ALPHA; ++ch) {
       if (t[ROOT].next[ch] != -1) {
         t[ROOT].go[ch] = t[ROOT].next[ch];
@@ -53,12 +54,13 @@ struct AhoCorasick {
       for (int ch = 0; ch < ALPHA; ++ch) {
         auto u = t[v].next[ch];
         if (u == -1) continue;
-        t[u].link = v == ROOT ? ROOT : t[t[v].link].go[ch];
+        t[u].suf_link = v == ROOT ? ROOT : t[t[v].suf_link].go[ch];
+        t[u].term_suf_link = !t[t[u].suf_link].ids.empty() ? t[u].suf_link : t[t[u].suf_link].term_suf_link;
         for (int d = 0; d < ALPHA; ++d) {
           if (t[u].next[d] != -1) {
             t[u].go[d] = t[u].next[d];
           } else {
-            t[u].go[d] = t[t[u].link].go[d];
+            t[u].go[d] = t[t[u].suf_link].go[d];
           }
         }
         q.emplace(u);
