c39b275b6878229c061e1bcb818e0823c7e89f63
aminjonshermatov <aminjonshermatov@gmail.com>
2023-10-06 21:05:58 +0300

    atcoder/dp_contest

--- atcoder/dp_contest/F.cpp
+++ atcoder/dp_contest/F.cpp
@@ -1,12 +1,4 @@
-// #define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("fast-math")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 
 #ifdef LOCAL
 #include "debug.h"
@@ -15,109 +7,74 @@
 #endif
 
 using namespace std;
-using namespace __gnu_pbds;
-
-typedef long long           ll;
-typedef long double         ld;
-typedef pair<ll, ll>        pll;
-typedef pair<int, int>      pii;
-typedef pair<ll, int>       pli;
-typedef pair<int, ll>       pil;
-typedef unsigned long long  ull;
-
-#define fi      first
-#define se      second
-#define P       pair
-#define mp      make_pair
-#define pb      push_back
-#define eb      emplace_back
-#define all(x)  (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x)   (ll)(x.size())
-
-#define rep(i, b)       for (auto i = 0; i < b; ++i)
-#define forr(el, cont)  for (auto &el : cont)
-
-template<typename T>             using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<typename K, typename V> using gp_ht = gp_hash_table<K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>, hash_standard_resize_policy<hash_exponential_size_policy<>, hash_load_check_resize_trigger<>, true>>;
-
-inline constexpr auto  inf = numeric_limits<int>::max();
-inline constexpr auto ninf = numeric_limits<int>::min();
-inline constexpr int MOD = 1e9 + 7;
-inline const ld pi = ::atan2(0, -1);
-inline constexpr ld eps = 1e-6;
-
-mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
 
 void solve() {
-    string s, t;
-    cin >> s >> t;
+  string s, t;
+  cin >> s >> t;
 
-    const int n(sz(s));
-    const int m(sz(t));
+  const auto n = int(s.size());
+  const auto m = int(t.size());
 
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    vector<vector<char>> prv(n + 1, vector<char>(m + 1, '#'));
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            dp[i][j] = max({(s[i - 1] == t[j - 1]) + dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]});
-            if (dp[i][j] == (s[i - 1] == t[j - 1]) + dp[i - 1][j - 1]) {
-                prv[i][j] = 'd';
-            } else if (dp[i][j] == dp[i - 1][j]) {
-                prv[i][j] = 'u';
-            } else {
-                prv[i][j] = 'l';
-            }
-        }
+  vector<int> dp(m + 1, 0);
+  vector prv(n + 1, vector<char>(m + 1, 'x'));
+  for (int i = 1; i <= n; ++i) {
+    vector<int> ndp(m + 1, 0);
+    for (int j = 1; j <= m; ++j) {
+      if (s[i - 1] == t[j - 1] && dp[j - 1] + 1 > ndp[j]) {
+        ndp[j] = dp[j - 1] + 1;
+        prv[i][j] = 'd';
+      }
+      if (dp[j] > ndp[j]) {
+        ndp[j] = dp[j];
+        prv[i][j] = 'u';
+      }
+      if (ndp[j - 1] > ndp[j]) {
+        ndp[j] = ndp[j - 1];
+        prv[i][j] = 'l';
+      }
     }
-
-    int mx = ninf;
-    pair<int, int> cur(-1, -1);
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            if (dp[i][j] > mx) {
-                cur = pair(i, j);
-                mx = dp[i][j];
-            }
-        }
+    dp.swap(ndp);
+  }
+  auto cur = pair(n, m);
+  for (int j = 0; j <= m; ++j) {
+    if (dp[j] > dp[cur.second]) {
+      cur.second = j;
     }
+  }
 
-    string z;
-    if (mx > 0) {
-        assert(cur.fi != -1 && cur.se != -1);
-        while (cur.fi != -1 && cur.se != -1 && prv[cur.fi][cur.se] != '#') {
-            switch (prv[cur.fi][cur.se]) {
-                case 'd': {
-                    if (s[cur.fi - 1] == t[cur.se - 1]) z.pb(s[cur.fi - 1]);
-                    --cur.fi;
-                    --cur.se;
-                    break ;
-                }
-                case 'u': {
-                    --cur.fi;
-                    break ;
-                }
-                case 'l': {
-                    --cur.se;
-                    break ;
-                }
-            }
-        }
-        reverse(all(z));
+  string res;
+  res.reserve(n);
+  while (cur.first >= 0 && cur.second >= 0) {
+    switch (prv[cur.first][cur.second]) {
+      case 'd':
+        res.push_back(s[cur.first - 1]);
+        --cur.first, --cur.second;
+        break;
+      case 'u':
+        --cur.first;
+        break;
+      case 'l':
+        --cur.second;
+        break;
+      case 'x':
+        goto done;
+      default:
+        assert(false);
     }
-    cout << z << '\n';
+  }
+  done:
+  reverse(res.begin(), res.end());
+  cout << res << '\n';
 }
 
 bool is_multi = false;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-    int T = 1;
-    if (is_multi) cin >> T;
-    for (int tc = 1; tc <= T; ++tc) solve();
-
-    return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int T = 1;
+  if (is_multi) cin >> T;
+  for (int tc = 1; tc <= T; ++tc) solve();
+  return 0;
 }
