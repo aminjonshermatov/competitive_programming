7036a2a590e5baa76ff70c5d8364b7e1e273b8ff
aminjonshermatov <aminjonshermatov@gmail.com>
2023-10-14 22:04:00 +0300

    templates/max_flow update

--- templates/max_flow.cpp
+++ templates/max_flow.cpp
@@ -2,77 +2,86 @@
 
 using namespace std;
 
-struct Dinic {
-  using i64 = int64_t;
+// Dinic's algorithm
+struct MaxFlow {
+    using i64 = int64_t;
+    static constexpr auto inf = numeric_limits<i64>::max();
 
-  struct FlowEdge {
-    int u, v;
-    i64 cap, flow;
-    FlowEdge(int u_, int v_, i64 cap_) : u(u_), v(v_), cap(cap_), flow(0) { }
-  };
+    struct FlowEdge {
+        int u, v;
+        i64 cap, flow;
+        int id;
+        FlowEdge(int u_, int v_, i64 cap_, int id_) : u(u_), v(v_), cap(cap_), flow(0), id(id_) { }
+    };
 
-  static constexpr auto inf = numeric_limits<i64>::max();
-  vector<FlowEdge> edges;
-  vector<vector<int>> g;
-  int n, S, T;
-  vector<int> level, ptr;
-  queue<int> q;
+    vector<FlowEdge> edges;
+    vector<vector<int>> g;
+    int n;
+    vector<int> level, ptr;
 
-  Dinic(int n_, int S_, int T_) : n(n_), S(S_), T(T_) {
-    g.resize(n);
-    level.resize(n);
-    ptr.resize(n);
-  }
-  void add_edge(int u, int v, i64 cap) {
-    g[u].emplace_back(edges.size());
-    edges.emplace_back(u, v, cap);
-    g[v].emplace_back(edges.size());
-    edges.emplace_back(v, u, 0);
-  }
-  bool bfs() {
-    while (!q.empty()) {
-      auto u = q.front();
-      q.pop();
-      for (auto i : g[u]) {
-        if (edges[i].cap - edges[i].flow < 1) continue;
-        auto v = edges[i].v;
-        if (level[v] != -1) continue;
-        level[v] = level[u] + 1;
-        q.emplace(v);
-      }
+    MaxFlow(int n_) : n(n_) {
+      g.resize(n);
+      level.resize(n);
+      ptr.resize(n);
     }
-    return level[T] != -1;
-  }
-  i64 dfs(int u, i64 pushed) {
-    if (pushed == 0 || u == T) {
-      return pushed;
+    void add_edge(int u, int v, i64 cap, int id) {
+      g[u].emplace_back(edges.size());
+      edges.emplace_back(u, v, cap, id);
+      g[v].emplace_back(edges.size());
+      edges.emplace_back(v, u, 0, -1);
     }
-    for (auto &pid = ptr[u]; pid < g[u].size(); ++pid) {
-      auto i = g[u][pid];
-      auto v = edges[i].v;
-      if (level[u] + 1 != level[v] || edges[i].cap - edges[i].flow < 1) continue;
-      auto f = dfs(v, min(pushed, edges[i].cap - edges[i].flow));
-      if (f == 0) continue;
-      edges[i].flow += f;
-      edges[i ^ 1].flow -= f;
-      return f;
-    }
-    return 0;
-  }
-  i64 flow() {
-    i64 f = 0;
-    for (;;) {
+    bool bfs(int S, int T) {
+      queue<int> q;
       fill(level.begin(), level.end(), -1);
       level[S] = 0;
       q.emplace(S);
-      if (!bfs()) {
-        break;
+      while (!q.empty()) {
+        auto u = q.front();
+        q.pop();
+        for (auto i : g[u]) {
+          if (edges[i].cap - edges[i].flow < 1) continue;
+          auto v = edges[i].v;
+          if (level[v] != -1) continue;
+          level[v] = level[u] + 1;
+          q.emplace(v);
+        }
       }
-      fill(ptr.begin(), ptr.end(), 0);
-      while (auto pushed = dfs(S, inf)) {
-        f += pushed;
-      }
+      return level[T] != -1;
     }
-    return f;
-  }
+    i64 dfs(int u, int T, i64 pushed) {
+      if (pushed == 0 || u == T) {
+        return pushed;
+      }
+      for (auto &pid = ptr[u]; pid < g[u].size(); ++pid) {
+        auto i = g[u][pid];
+        auto v = edges[i].v;
+        if (level[u] + 1 != level[v] || edges[i].cap - edges[i].flow < 1) continue;
+        auto f = dfs(v, T, min(pushed, edges[i].cap - edges[i].flow));
+        if (f == 0) continue;
+        edges[i].flow += f;
+        edges[i ^ 1].flow -= f;
+        return f;
+      }
+      return 0;
+    }
+
+    decltype(auto) flow(int S, int T) {
+      i64 f = 0;
+      for (;;) {
+        if (!bfs(S, T)) {
+          break;
+        }
+        fill(ptr.begin(), ptr.end(), 0);
+        while (auto pushed = dfs(S, T, inf)) {
+          f += pushed;
+        }
+      }
+      map<int, i64> fall_through;
+      for (const auto &edge : edges) {
+        if (edge.id >= 0 && edge.flow > 0) {
+          fall_through[edge.id] = edge.flow;
+        }
+      }
+      return tuple{f, fall_through};
+    }
 };
