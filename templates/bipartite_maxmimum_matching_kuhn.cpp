c1f7d90496b16e8e8438eaaed9e8bf5fc057a30d
aminjonshermatov <aminjonshermatov@gmail.com>
2023-10-25 21:39:06 +0300

    maximum matching bipartite | random

--- templates/bipartite_maxmimum_matching_kuhn.cpp
+++ templates/bipartite_maxmimum_matching_kuhn.cpp
@@ -3,34 +3,37 @@
 //
 #include <bits/stdc++.h>
 
-using namespace std;
-
 // Maximum matching in bipartite graph
 // Kuhn's Algorithm
 template <typename G>
-decltype(auto) MaximumMatchingBipartite(int nA, int nB, const G& g) {
-  vector<int> mt(nB, -1), tm(nA, -1), when(nA, 0);
+decltype(auto) MaximumMatchingBipartite(int nA, int nB, G& g) {
+  std::vector<int> mt(nB, -1), tm(nA, -1), when(nA, 0);
 
   int timer = 0;
   const auto try_kuhn = [&](auto &f, int v) -> bool {
-      if (when[v] == timer) return false;
-      when[v] = timer;
-      for (auto u : g[v]) {
-        if (mt[u] == -1) {
-          tm[v] = u;
-          mt[u] = v;
-          return true;
-        }
+    if (when[v] == timer) return false;
+    when[v] = timer;
+    for (auto u : g[v]) {
+      if (mt[u] == -1) {
+        tm[v] = u;
+        mt[u] = v;
+        return true;
       }
-      for (auto u : g[v]) {
-        if (mt[u] != -1 && f(f, mt[u])) {
-          tm[v] = u;
-          mt[u] = v;
-          return true;
-        }
+    }
+    for (auto u : g[v]) {
+      if (mt[u] != -1 && f(f, mt[u])) {
+        tm[v] = u;
+        mt[u] = v;
+        return true;
       }
-      return false;
+    }
+    return false;
   };
+
+  std::mt19937 mt19937(std::chrono::steady_clock::now().time_since_epoch().count());
+  for (int v = 0; v < nA; ++v) {
+    std::shuffle(g[v].begin(), g[v].end(), mt19937);
+  }
 
   for (bool run = true; run;) {
     run = false;
@@ -42,7 +45,7 @@ decltype(auto) MaximumMatchingBipartite(int nA, int nB, const G& g) {
     }
   }
 
-  vector<pair<int, int>> edges;
+  std::vector<std::pair<int, int>> edges;
   for (int v = 0; v < nB; ++v) {
     if (mt[v] != -1) {
       edges.emplace_back(mt[v], v);
