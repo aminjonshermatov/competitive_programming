82e897a5c7b29e390b907315aa505061263e7aa5
aminjonshermatov <aminjonshermatov@gmail.com>
2023-10-28 23:36:50 +0300

    templates/sieve

--- templates/primes.cpp
+++ templates/primes.cpp
@@ -3,36 +3,23 @@
 //
 #include <bits/stdc++.h>
 
-using namespace std;
-
-template<typename T> struct Primes {
-    T n;
-    vector<T> lp, pr;
-
-    using iterator          = typename vector<T>::iterator;
-    using const_iterator    = typename vector<T>::const_iterator;
-
-    explicit Primes(T n_) : n(n_) { lp.assign(n + 1, T(0)); find_primes(); }
-
-    auto find_primes() -> void {
-        for (T i = 2; i <= n; ++i) {
-            if (lp[i] == 0) {
-                lp[i] = i;
-                pr.push_back(i);
-            }
-            for (T j = 0; i * pr[j] <= n; ++j) {
-                lp[i * pr[j]] = pr[j];
-                if (pr[j] == lp[i]) break;
-            }
-        }
+// Linear Sieve
+decltype(auto) Sieve(int n) {
+  std::vector<int> spf(n + 1, 1), primes;
+  for (int i = 2; i <= n; ++i) {
+    if (spf[i] == 1) {
+      spf[i] = i;
+      primes.emplace_back(i);
     }
-
-    T  operator[](int idx) { return pr[idx]; }
-    T& operator[](int idx) const { return pr[idx]; }
-    [[nodiscard]] size_t size() const { return pr.size(); }
-
-    iterator begin() { return pr.begin(); }
-    [[nodiscard]] const_iterator begin() const { return pr.begin(); }
-    iterator end() { return pr.end(); }
-    [[nodiscard]] const_iterator end() const { return pr.end(); }
-};
+    for (auto p : primes) {
+      if (i * p > n) {
+        break;
+      }
+      spf[i * p] = p;
+      if (p == spf[i]) {
+        break;
+      }
+    }
+  }
+  return std::tuple(primes, spf);
+}
