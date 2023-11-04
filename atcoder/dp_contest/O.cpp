a7cf64c26ac7fd645cb4b4f01d6c23c783ce6c60
aminjonshermatov <aminjonshermatov@gmail.com>
2023-11-04 22:09:54 +0300

    atcoder/dp/O

---
+++ atcoder/dp_contest/O.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...) 42
+#endif
+
+using i64 = int64_t;
+
+template <typename T> T power(T a, i64 b) noexcept {
+  T res = 1;
+  for (; b > 0; b >>= 1, a *= a) {
+    if (b & 1) {
+      res *= a;
+    }
+  }
+  return res;
+}
+
+template <i64 P> struct Z {
+  i64 x = 0;
+  constexpr Z() noexcept = default;
+  constexpr Z(i64 x) noexcept : x(norm(x % getMod())) { }
+
+  static i64 getMod() noexcept { return P; }
+  [[nodiscard]] constexpr i64 norm(i64 y) const noexcept {
+    if (y < 0) {
+      y += getMod();
+    }
+    if (y >= getMod()) {
+      y -= getMod();
+    }
+    return y;
+  }
+
+  [[nodiscard]] constexpr i64 val() const noexcept { return x; }
+  constexpr explicit operator i64() const noexcept { return x; }
+
+  constexpr Z operator-() const noexcept {
+    return Z(getMod() - x);
+  }
+  [[nodiscard]] constexpr Z inv() const noexcept {
+    return power(*this, getMod() - 2);
+  }
+
+  constexpr Z& operator+=(Z other) noexcept {
+    x = norm(x + other.val());
+    return *this;
+  }
+  constexpr Z& operator-=(Z other) noexcept {
+    x = norm(x - other.val());
+    return *this;
+  }
+  constexpr Z& operator*=(Z other) noexcept {
+    x = norm((x * other.val()) % getMod());
+    return *this;
+  }
+  constexpr Z& operator/=(Z other) noexcept {
+    return *this *= other.inv();
+  }
+
+  constexpr Z operator+(Z other) const noexcept {
+    return {x + other.val()};
+  }
+  constexpr Z operator-(Z other) const noexcept {
+    return {norm(x - other.val())};
+  }
+  constexpr Z operator*(Z other) const noexcept {
+    return {norm((x * other.val()) % getMod())};
+  }
+  constexpr Z operator/(Z other) const noexcept {
+    return {other.inv() * x};
+  }
+
+  constexpr bool operator==(Z other) const noexcept {
+    return val() == other.val();
+  }
+  constexpr bool operator!=(Z other) const noexcept {
+    return val() != other.val();
+  }
+
+  template<i64 Mod> friend std::istream& operator>>(std::istream &in, Z<Mod> &z) {
+    i64 y;
+    in >> y;
+    z = Z{y};
+    return in;
+  }
+  template<i64 Mod> friend std::ostream& operator<<(std::ostream &out, const Z<Mod> &z) {
+    return out << z.val();
+  }
+};
+
+constexpr i64 Mod = 1e9 + 7;
+using Mint = Z<Mod>;
+
+void solve() {
+  int n;
+  std::cin >> n;
+  std::vector g(n, std::vector<int>(n));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      std::cin >> g[i][j];
+    }
+  }
+
+  std::array<std::vector<Mint>, 2> dp{};
+  for (auto &r : dp) {
+    r.resize(1 << n);
+  }
+  int cur = 0;
+  dp[cur][0] = 1;
+  for (int i = 0; i < n; ++i, cur ^= 1) {
+    for (uint m1 = 0; m1 < (1 << n); ++m1) {
+      for (uint m2 = (~m1 & ((1 << n) - 1)); m2 > 0; m2 -= m2 & -m2) {
+        auto j = 31 - __builtin_clz(m2 & -m2);
+        if (g[i][j] == 0) continue;
+        dp[cur ^ 1][m1 | (1 << j)] += dp[cur][m1];
+      }
+    }
+  }
+  std::cout << dp[cur].back() << '\n';
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  solve();
+}
