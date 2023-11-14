f797cfbc921a26c6e34c57779ea66d2d2552db84
aminjonshermatov <aminjonshermatov@gmail.com>
2023-11-14 23:16:39 +0300

    templates/rmq | update

--- templates/sparse_table.cpp
+++ templates/sparse_table.cpp
@@ -4,12 +4,11 @@
 
 #include <bits/stdc++.h>
 
-template <typename T, typename Op = std::function<T(const T, const T)>> struct SparseTable {
-  Op op;
-  std::vector<std::vector<T>> table;
+template <typename Node> struct SparseTable {
+  std::vector<std::vector<Node>> table;
   std::vector<int> lg;
 
-  template<typename U> auto init(const std::vector<U> &init_) -> void {
+  template <typename U> auto init(const std::vector<U> &init_) -> void {
     table.assign(init_.size(), {});
     lg.resize(init_.size() + 1);
     lg[0] = -1;
@@ -17,20 +16,27 @@ template <typename T, typename Op = std::function<T(const T, const T)>> struct S
       lg[i] = 31 - __builtin_clz(i);
     }
     for (auto i = 0u; i < init_.size(); ++i) {
-      table[i] = {init_[i]};
+      table[i].push_back({init_[i]});
     }
     for (auto l = 1u, j = 0u; 2 * l <= init_.size(); ++j, l <<= 1) {
       for (auto i = 0u; i + 2 * l <= init_.size(); ++i) {
-        table[i].emplace_back(op(table[i][j], table[i + l][j]));
+        table[i].emplace_back(Node::unite(table[i][j], table[i + l][j]));
       }
     }
   }
 
-  template<typename U> explicit SparseTable(const std::vector<U> &init_, Op &&op_) : op(op_) { init(init_); }
+  template <typename U> explicit SparseTable(const std::vector<U> &init_) { init(init_); }
 
-  [[nodiscard]] auto query(int l, int r) const -> int {
+  [[nodiscard]] decltype(auto) query(int l, int r) const {
     assert(0 <= l && l < r && r <= table.size());
     const auto b = lg[r - l];
-    return op(table[l][b], table[r - (1 << b)][b]);
+    return Node::unite(table[l][b], table[r - (1 << b)][b]);
+  }
+};
+
+struct Node {
+  int val = std::numeric_limits<int>::max();
+  static Node unite(Node a, Node b) {
+    return a.val < b.val ? a : b;
   }
 };
