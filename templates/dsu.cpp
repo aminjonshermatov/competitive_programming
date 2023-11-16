ff1b9202cd1628019f3c39a2c3480e2adb9f67f5
aminjonshermatov <aminjonshermatov@gmail.com>
2023-11-17 00:33:17 +0300

    templates/dsu weighted dsu

--- templates/dsu.cpp
+++ templates/dsu.cpp
@@ -13,34 +13,35 @@ struct Dsu {
 
   inline int find(int v) {
     assert(0 <= v && v < n);
-    if (parent[v] != v) {
-      parent[v] = find(parent[v]);
+
+    while (v != parent[v]) {
+      v = parent[v] = parent[parent[v]];
     }
-    return parent[v];
+    return v;
   }
 
   bool merge(int u, int v) {
-    assert(0 <= v && v < n);
     assert(0 <= u && u < n);
-    auto pv = find(v);
+    assert(0 <= v && v < n);
     auto pu = find(u);
-    if (pv == pu) {
+    auto pv = find(v);
+    if (pu == pv) {
       return false;
     }
 
-    if (rank[pv] < rank[pu]) {
-      std::swap(pv, pu);
+    if (rank[pu] < rank[pv]) {
+      std::swap(pu, pv);
     }
-    parent[pu] = pv;
-    rank[pv] += rank[pu];
+    parent[pv] = pu;
+    rank[pu] += rank[pv];
     --components;
     return true;
   }
 
   inline bool is_same(int u, int v) {
-    assert(0 <= v && v < n);
     assert(0 <= u && u < n);
-    return find(v) == find(u);
+    assert(0 <= v && v < n);
+    return find(u) == find(v);
   }
 };
 
@@ -57,37 +58,37 @@ struct DsuRollback {
 
   inline int find(int v) {
     assert(0 <= v && v < n);
-    if (parent[v] != v) {
+    while (v != parent[v]) {
       history.emplace_back(UpdateType::kUpdateParent, v, parent[v]);
-      parent[v] = find(parent[v]);
+      v = parent[v] = parent[parent[v]];
     }
-    return parent[v];
+    return v;
   }
 
   bool merge(int u, int v) {
-    assert(0 <= v && v < n);
     assert(0 <= u && u < n);
-    auto pv = find(v);
+    assert(0 <= v && v < n);
     auto pu = find(u);
-    if (pv == pu) {
+    auto pv = find(v);
+    if (pu == pv) {
       return false;
     }
 
-    if (rank[pv] < rank[pu]) {
-      std::swap(pv, pu);
+    if (rank[pu] < rank[pv]) {
+      std::swap(pu, pv);
     }
-    history.emplace_back(UpdateType::kUpdateParent, pu, parent[pu]);
-    parent[pu] = pv;
-    history.emplace_back(UpdateType::kUpdateRank, pv, rank[pv]);
-    rank[pv] += rank[pu];
+    history.emplace_back(UpdateType::kUpdateParent, pv, parent[pv]);
+    parent[pv] = pu;
+    history.emplace_back(UpdateType::kUpdateRank, pu, rank[pu]);
+    rank[pu] += rank[pv];
     --components;
     return true;
   }
 
   inline bool is_same(int u, int v) {
-    assert(0 <= v && v < n);
     assert(0 <= u && u < n);
-    return find(v) == find(u);
+    assert(0 <= v && v < n);
+    return find(u) == find(v);
   }
 
   void rollback() {
@@ -111,5 +112,72 @@ struct DsuRollback {
       parent[v] = old_parent;
     }
     ++components;
+  }
+};
+
+template <typename Node> struct DsuWeighted {
+  int n, components;
+  std::vector<int> parent, rank;
+  std::vector<Node> weight;
+
+  explicit DsuWeighted(int n_) : n(n_), components(n), parent(n), rank(n, 1), weight(n, Node()) {
+    iota(parent.begin(), parent.end(), 0);
+  }
+
+  inline decltype(auto) find(int v) {
+    assert(0 <= v && v < n);
+    auto res = Node();
+
+    while (v != parent[v]) {
+      res = Node::unite(res, weight[v]);
+      res = Node::unite(res, weight[parent[v]]);
+      weight[v] = Node::unite(weight[v], weight[parent[v]]);
+      v = parent[v] = parent[parent[v]];
+    }
+    return std::pair(v, res);
+  }
+
+  bool merge(int u, int v, Node w) {
+    assert(0 <= u && u < n);
+    assert(0 <= v && v < n);
+    auto [pu, wu] = find(u);
+    auto [pv, wv] = find(v);
+    if (pu == pv) {
+      return false;
+    }
+
+    if (rank[pu] < rank[pv]) {
+      std::swap(pv, pu);
+      std::swap(wv, wu);
+      w = Node::inverse(w);
+    }
+    // U - V = w
+    // weight[v] - weight[u] + ? = w
+    // ? = weight[u] - weight[v] + w
+    parent[pv] = pu;
+    rank[pu] += rank[pv];
+    weight[pv] = Node::unite(wu, Node::unite(Node::inverse(wv), w));
+    --components;
+    return true;
+  }
+
+  inline bool is_same(int u, int v) {
+    assert(0 <= u && u < n);
+    assert(0 <= v && v < n);
+    return find(u).first == find(v).first;
+  }
+};
+
+using i64 = int64_t;
+struct Node {
+  i64 val = 0;
+  inline static Node unite(Node a, Node b) {
+    return Node{a.val + b.val};
+  }
+  inline static Node inverse(Node a) {
+    return Node{-a.val};
+  }
+  inline bool operator==(const Node &node) const noexcept {
+    return val == node.val;
   }
 };
