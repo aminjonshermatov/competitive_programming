927b13aeec8097ea1f1cfccbbcf5e7db2a47ad9d
aminjonshermatov <aminjonshermatov@gmail.com>
2023-12-09 18:13:14 +0300

    templates/xor_basis attributes

--- templates/xor_basis.cpp
+++ templates/xor_basis.cpp
@@ -8,14 +8,16 @@ struct XorBasis {
   std::array<T, B> basis{};
   int tried, added;
   XorBasis() : tried(0), added(0) { basis.fill(-1); }
-  void add(T x) {
+  __attribute__((target("avx2"), optimize("O3", "unroll-loops"))) void add(T x) {
     ++tried;
     for (; x > 0;) {
       int ib;
       if constexpr (std::is_same<T, int32_t>()) {
         ib = sizeof(T) * __CHAR_BIT__  - 1 - __builtin_clz(x);
-      } else {
+      } else if constexpr (std::is_same<T, int64_t>()) {
         ib = sizeof(T) * __CHAR_BIT__  - 1 - __builtin_clzll(x);
+      } else {
+        ib = std::log2(x);
       }
       if (basis[ib] == -1) {
         ++added;
@@ -25,13 +27,15 @@ struct XorBasis {
     }
   }
   // returns count number of subsets that xor of elements equal to `x`, count = 2 ^ ret, or -1 if it doesn't exist
-  [[nodiscard]] int subsetCount(T x) const {
+  __attribute__((target("avx2"), optimize("O3", "unroll-loops"))) [[nodiscard]] int subsetCount(T x) const {
     for (; x > 0;) {
       int ib;
       if constexpr (std::is_same<T, int32_t>()) {
         ib = sizeof(T) * __CHAR_BIT__  - 1 - __builtin_clz(x);
-      } else {
+      } else if constexpr (std::is_same<T, int64_t>()) {
         ib = sizeof(T) * __CHAR_BIT__  - 1 - __builtin_clzll(x);
+      } else {
+        ib = std::log2(x);
       }
       if (basis[ib] == -1) break;
       x ^= basis[ib];
@@ -39,7 +43,7 @@ struct XorBasis {
     return x == 0 ? tried - added : -1;
   }
   // let F(S) = xor of all elements in S, return max value F(S'), where S' is all possible subsets of S
-  [[nodiscard]] T maximumXor() const {
+  __attribute__((target("avx2"), optimize("O3", "unroll-loops"))) [[nodiscard]] T maximumXor() const {
     T max = 0;
     for (int ib = B - 1; ib >= 0; --ib) {
       if (basis[ib] == -1 || max >> ib & 1) continue;
