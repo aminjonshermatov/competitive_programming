d89c627984497a43105b220bf48a6553898825ba
aminjonshermatov <aminjonshermatov@gmail.com>
2023-12-28 00:33:55 +0300

    cses/Advanced/HousesAndSchools_tle_li_chao | yet another optimization

--- cses/AdvancedTechniques/main.cpp
+++ cses/AdvancedTechniques/main.cpp
@@ -12,15 +12,17 @@
 #define dbg(...) 42
 #endif
 
-using i64 = int64_t;
-constexpr auto inf = std::numeric_limits<i64>::max() / 2;
-constexpr int N = 3002;
-static i64 cs[N], pf[N], sf[N], pfComplex[N], sfComplex[N], cost[N * N], dp[2][N];
-static int nodes[N * 4], nxt, n;
+using i64 = int_fast64_t;
+using i32 = int_fast32_t;
 
-inline i64 eval(int L, int x) { return L == -1 ? inf : cost[L * N + x] + dp[nxt ^ 1][L - 1]; }
-inline void add(int L) {
-  int id = 0, lb = 0, rb = n;
+constexpr auto inf = std::numeric_limits<i64>::max() / 2;
+constexpr i32 N = 3002;
+static i64 cs[N], pf[N], sf[N], pfComplex[N], sfComplex[N], cost[N * N], dp[2][N];
+static i32 nodes[N * 4], nxt, n;
+
+inline i64 eval(i32 L, i32 x) { return L == -1 ? inf : cost[L * N + x] + dp[nxt ^ 1][L - 1]; }
+inline void add(i32 L) {
+  i32 id = 0, lb = 0, rb = n;
   for (;;) {
     auto mid = lb + (rb - lb) / 2;
 
@@ -38,8 +40,8 @@ inline void add(int L) {
     (fLeft != fMid ? rb : lb) = mid;
   }
 }
-inline i64 qry(int x) {
-  int id = 0, lb = 0, rb = n;
+inline i64 qry(i32 x) {
+  i32 id = 0, lb = 0, rb = n;
   auto ret = inf;
   for (;;) {
     ret = std::min(ret, eval(nodes[id], x));
@@ -51,31 +53,31 @@ inline i64 qry(int x) {
     (x < mid ? rb : lb) = mid;
   }
 }
-inline i64 C(int l, int m, int r) {
+inline i64 C(i32 l, i32 m, i32 r) {
   return pfComplex[m] - pfComplex[l - 1] - pf[l - 1] * (m - l + 1) + sfComplex[m] - sfComplex[r + 1] - sf[r + 1] * (r - m + 1);
 }
 
 void solve() {
-  int k;
+  i32 k;
   std::cin >> n >> k;
-  for (int i = 1; i <= n; ++i) {
+  for (i32 i = 1; i <= n; ++i) {
     std::cin >> cs[i];
   }
 
   pf[0] = pfComplex[0] = 0;
-  for (int i = 1; i <= n; ++i) {
+  for (i32 i = 1; i <= n; ++i) {
     pfComplex[i] = pfComplex[i - 1] + pf[i - 1];
     pf[i] = pf[i - 1] + cs[i];
   }
   sf[n + 1] = sfComplex[n + 1] = 0;
-  for (int i = n; i >= 1; --i) {
+  for (i32 i = n; i >= 1; --i) {
     sfComplex[i] = sfComplex[i + 1] + sf[i + 1];
     sf[i] = sf[i + 1] + cs[i];
   }
 
-  for (int l = 1; l <= n; ++l) {
-    int best_mid = l;
-    for (int r = l; r <= n; ++r) {
+  for (i32 l = 1; l <= n; ++l) {
+    i32 best_mid = l;
+    for (i32 r = l; r <= n; ++r) {
       while (best_mid < r && C(l, best_mid, r) > C(l, best_mid + 1, r)) {
         ++best_mid;
       }
@@ -84,11 +86,11 @@ void solve() {
   }
 
   nxt = 1;
-  std::fill(dp[nxt ^ 1], dp[nxt ^ 1] + n + 2, inf);
+  std::fill(dp[nxt ^ 1], dp[nxt ^ 1] + n + 1, inf);
   dp[nxt ^ 1][0] = 0;
-  for (int _ = 1; _ <= k; ++_, nxt ^= 1) {
+  for (i32 _ = 1; _ <= k; ++_, nxt ^= 1) {
     std::memset(nodes, -1, sizeof nodes);
-    for (int r = _; r <= n; ++r) {
+    for (i32 r = _; r <= n; ++r) {
       add(r);
       dp[nxt][r] = qry(r);
     }
@@ -96,7 +98,7 @@ void solve() {
   std::cout << dp[nxt ^ 1][n] << '\n';
 }
 
-int main() {
+int32_t main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
