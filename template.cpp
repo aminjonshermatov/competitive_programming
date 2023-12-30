99605518a65992981fa779c88292553440289276
aminjonshermatov <aminjonshermatov@gmail.com>
2023-12-30 16:29:38 +0300

    template | pragmas

--- template.cpp
+++ template.cpp
@@ -3,10 +3,11 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
-#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
 #pragma GCC optimize("unroll-loops")
 #pragma GCC optimize("fast-math")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("tree-vectorize")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma,tune=native")
 
 #ifdef LOCAL
 #include "debug.h"
