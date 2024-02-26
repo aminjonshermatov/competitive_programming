43de5b6de33367f880b0b6f1b0973d65a0d23126
aminjonshermatov <aminjonshermatov@gmail.com>
2024-02-26 15:40:49 +0300

    templates/fraction

--- templates/fraction.cpp
+++ templates/fraction.cpp
@@ -11,33 +11,39 @@ template <typename T> class Fraction {
     denominator /= g;
   }
 public:
-  explicit Fraction(T numerator_ = T(0), T denominator_ = T(1)) : numerator(numerator_), denominator(denominator_) { assert(denominator != T(0)); reduce(); }
+  explicit Fraction(T numerator_ = T(0), T denominator_ = T(1)) : numerator(numerator_), denominator(denominator_) {
+    assert(denominator != T(0));
+    reduce();
+  }
 
   Fraction operator-() const {
     return Fraction(-numerator, denominator);
   }
 
-  Fraction operator+(const Fraction<T> &other) const {
+  Fraction operator+(const Fraction<T>& other) const {
     return Fraction(numerator * other.denominator + denominator * other.numerator, denominator * other.denominator);
   }
-  Fraction operator-(const Fraction<T> &other) const {
+  Fraction operator-(const Fraction<T>& other) const {
     return Fraction(numerator * other.denominator - denominator * other.numerator, denominator * other.denominator);
   }
-  Fraction operator*(const Fraction<T> &other) const {
+  Fraction operator*(const Fraction<T>& other) const {
     return Fraction(numerator * other.numerator, denominator * other.denominator);
   }
-  Fraction operator/(const Fraction<T> &other) const {
+  Fraction operator/(const Fraction<T>& other) const {
     return Fraction(numerator * other.denominator, denominator * other.numerator);
   }
 
-  bool operator<(const Fraction<T> &other) const {
+  bool operator<(const Fraction<T>& other) const {
     return numerator * other.denominator < denominator * other.numerator;
   }
-  bool operator>(const Fraction<T> &other) const {
+  bool operator>(const Fraction<T>& other) const {
     return numerator * other.denominator > denominator * other.numerator;
   }
-  bool operator==(const Fraction<T> &other) const {
+  bool operator==(const Fraction<T>& other) const {
     return numerator == other.numerator && denominator == other.denominator;
+  }
+  bool operator!=(const Fraction<T>& other) const {
+    return numerator != other.numerator || denominator != other.denominator;
   }
 
   [[nodiscard]] long double toLongDouble() const {
@@ -45,17 +51,17 @@ public:
   }
 
   [[maybe_unused]] static constexpr char DELIMETER = '/';
-  template <typename U> friend std::istream& operator>>(std::istream &in, Fraction<U> &fraction) {
-    in >> fraction.numerator;
+  template <typename U> friend std::istream& operator>>(std::istream& is, Fraction<U>& fraction) {
+    is >> fraction.numerator;
     char _;
-    in >> _;
+    is >> _;
     assert(_ == DELIMETER);
-    in >> fraction.denominator;
+    is >> fraction.denominator;
     assert(fraction.denominator != U(0));
-    return in;
+    return is;
   }
-  template <typename U> friend std::ostream& operator<<(std::ostream &out, const Fraction<U> &fraction) {
-    return out << fraction.numerator << DELIMETER << fraction.denominator;
+  template <typename U> friend std::ostream& operator<<(std::ostream& os, const Fraction<U>& fraction) {
+    return os << fraction.numerator << DELIMETER << fraction.denominator;
   }
 };
 using Frac = Fraction<int>;
