074ae185de2e1c9fdfbd039faacdab2a43e30bbb
aminjonshermatov <aminjonshermatov@gmail.com>
2024-03-16 00:10:11 +0300

    templates/segment_tree | lvalue optimisation

--- templates/segment_tree.cpp
+++ templates/segment_tree.cpp
@@ -5,36 +5,36 @@
 
 using namespace std;
 
-template <typename Node, typename Tag>
-struct LazySegmentTree {
+template <typename Node, typename Tag> struct LazySegmentTree {
   int n = 0;
   std::vector<Node> nodes;
   std::vector<Tag> tags;
 
+  LazySegmentTree() = default;
   explicit LazySegmentTree(int n_) { init(std::vector(n_, Node())); }
-  LazySegmentTree(int n_, Node node) { init(std::vector(n_, node)); }
-  template <typename T> explicit LazySegmentTree(std::vector<T> init_) { init(init_); }
+  LazySegmentTree(int n_, const Node& node) { init(std::vector(n_, node)); }
+  template <typename T> explicit LazySegmentTree(const std::vector<T>& init_) { init(init_); }
 
-  template <typename T> void init(std::vector<T> init_) {
+  template <typename T> void init(const std::vector<T>& init_) {
     for (n = 1; n < init_.size(); n <<= 1) { }
     nodes.assign(2 * n - 1, Node());
     tags.assign(2 * n - 1, Tag());
-    auto build = [&](auto &f, int x, int lx, int rx) -> void {
+    auto build = [&](auto&& self, int x, int lx, int rx) -> void {
       if (rx - lx == 1) {
         if (lx < init_.size()) {
           nodes[x] = Node{init_[lx]};
         }
         return;
       }
-      auto mid = lx + (rx - lx) / 2;
-      f(f, 2 * x + 1, lx, mid);
-      f(f, 2 * x + 2, mid, rx);
+      const auto mid = lx + (rx - lx) / 2;
+      self(self, 2 * x + 1, lx, mid);
+      self(self, 2 * x + 2, mid, rx);
       nodes[x] = Node::unite(nodes[2 * x + 1], nodes[2 * x + 2]);
     };
     build(build, 0, 0, n);
   }
 
-  inline void apply(int x, const Tag &tag) {
+  inline void apply(int x, const Tag& tag) {
     nodes[x].apply(tag);
     tags[x].apply(tag);
   }
@@ -47,13 +47,13 @@ struct LazySegmentTree {
     nodes[x] = Node::unite(nodes[2 * x + 1], nodes[2 * x + 2]);
   }
 
-  void modify(int pos, Node node, int x, int lx, int rx) {
+  void modify(int pos, const Node& node, int x, int lx, int rx) {
     if (rx - lx == 1) {
       nodes[x] = node;
       return;
     }
     push(x);
-    auto mid = lx + (rx - lx) / 2;
+    const auto mid = lx + (rx - lx) / 2;
     if (pos < mid) {
       modify(pos, node, 2 * x + 1, lx, mid);
     } else {
@@ -61,39 +61,51 @@ struct LazySegmentTree {
     }
     pull(x);
   }
-  void modify(int pos, Node node) {
+  void modify(int pos, const Node& node) {
     modify(pos, node, 0, 0, n);
   }
-  void modify(int ql, int qr, Tag tag, int x, int lx, int rx) {
+  void modify(int ql, int qr, const Tag& tag, int x, int lx, int rx) {
     if (ql >= rx || qr <= lx) return;
     if (ql <= lx && rx <= qr) {
       apply(x, tag);
       return;
     }
     push(x);
-    auto mid = lx + (rx - lx) / 2;
+    const auto mid = lx + (rx - lx) / 2;
     modify(ql, qr, tag, 2 * x + 1, lx, mid);
     modify(ql, qr, tag, 2 * x + 2, mid, rx);
     pull(x);
   }
-  void modify(int ql, int qr, Tag tag) {
+  void modify(int ql, int qr, const Tag& tag) {
     modify(ql, qr, tag, 0, 0, n);
   }
   Node query(int ql, int qr, int x, int lx, int rx) {
-    if (ql >= rx || qr <= lx) return Node();
-    if (ql <= lx && rx <= qr) return nodes[x];
+    if (ql >= rx || qr <= lx) {
+      return Node();
+    }
+    if (ql <= lx && rx <= qr) {
+      return nodes[x];
+    }
     push(x);
-    auto mid = lx + (rx - lx) / 2;
+    const auto mid = lx + (rx - lx) / 2;
     return Node::unite(query(ql, qr, 2 * x + 1, lx, mid), query(ql, qr, 2 * x + 2, mid, rx));
   }
-  Node query(int ql, int qr) { return query(ql, qr, 0, 0, n); }
-  Node query(int pos) { return query(pos, pos + 1, 0, 0, n); }
+  Node query(int ql, int qr) {
+    return query(ql, qr, 0, 0, n);
+  }
+  Node query(int pos) {
+    return query(pos, pos + 1, 0, 0, n);
+  }
 
   template <typename F> int findFirst(int ql, int qr, F&& pred, int x, int lx, int rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) return -1;
-    if (rx - lx == 1) return lx;
+    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
+      return -1;
+    }
+    if (rx - lx == 1) {
+      return lx;
+    }
     push(x);
-    auto mid = lx + (rx - lx) / 2;
+    const auto mid = lx + (rx - lx) / 2;
     auto res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
     if (res == -1) {
       res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
@@ -104,10 +116,14 @@ struct LazySegmentTree {
     return findFirst(ql, qr, std::forward<F>(pred), 0, 0, n);
   }
   template <typename F> int findLast(int ql, int qr, F&& pred, int x, int lx, int rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) return -1;
-    if (rx - lx == 1) return lx;
+    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
+      return -1;
+    }
+    if (rx - lx == 1) {
+      return lx;
+    }
     push(x);
-    auto mid = lx + (rx - lx) / 2;
+    const auto mid = lx + (rx - lx) / 2;
     auto res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
     if (res == -1) {
       res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
@@ -120,19 +136,19 @@ struct LazySegmentTree {
 };
 
 struct Tag {
-  int64_t val = 0;
-  void apply(const Tag &tag) {
+  int val = -1;
+  void apply(const Tag& tag) {
     val += tag.val;
   }
 };
 
 struct Node {
-  int64_t val = 0;
-  void apply(const Tag &tag) {
-    val += tag.val;
+  int sum = 0;
+  void apply(const Tag& tag) {
+    sum += tag.val;
   }
-  static Node unite(Node a, Node b) {
-    return Node{a.val + b.val};
+  static Node unite(const Node& a, const Node& b) {
+    return {a.sum + b.sum};
   }
 };
 
@@ -141,34 +157,35 @@ template <typename Node> struct SegmentTree {
   int n = 0;
   std::vector<Node> nodes;
 
+  SegmentTree() = default;
   explicit SegmentTree(int n_) { init(std::vector(n_, Node())); }
-  SegmentTree(int n_, Node node) { init(std::vector(n_, node)); }
-  template <typename T> explicit SegmentTree(std::vector<T> init_) { init(init_); }
+  SegmentTree(int n_, const Node& node) { init(std::vector(n_, node)); }
+  template <typename T> explicit SegmentTree(const std::vector<T>& init_) { init(init_); }
 
-  template <typename T> void init(std::vector<T> init_) {
+  template <typename T> void init(const std::vector<T>& init_) {
     for (n = 1; n < init_.size(); n <<= 1) { }
     nodes.assign(2 * n - 1, Node());
-    auto build = [&](auto &f, int x, int lx, int rx) -> void {
+    auto build = [&](auto&& self, int x, int lx, int rx) -> void {
       if (rx - lx == 1) {
         if (lx < init_.size()) {
           nodes[x] = Node{init_[lx]};
         }
         return;
       }
-      auto mid = lx + (rx - lx) / 2;
-      f(f, 2 * x + 1, lx, mid);
-      f(f, 2 * x + 2, mid, rx);
+      const auto mid = lx + (rx - lx) / 2;
+      self(self, 2 * x + 1, lx, mid);
+      self(self, 2 * x + 2, mid, rx);
       nodes[x] = Node::unite(nodes[2 * x + 1], nodes[2 * x + 2]);
     };
     build(build, 0, 0, n);
   }
 
-  void modify(int pos, Node node, int x, int lx, int rx) {
+  void modify(int pos, const Node& node, int x, int lx, int rx) {
     if (rx - lx == 1) {
       nodes[x] = node;
       return;
     }
-    auto mid = lx + (rx - lx) / 2;
+    const auto mid = lx + (rx - lx) / 2;
     if (pos < mid) {
       modify(pos, node, 2 * x + 1, lx, mid);
     } else {
@@ -176,21 +193,33 @@ template <typename Node> struct SegmentTree {
     }
     nodes[x] = Node::unite(nodes[2 * x + 1], nodes[2 * x + 2]);
   }
-  void modify(int pos, Node node) {
+  void modify(int pos, const Node& node) {
     modify(pos, node, 0, 0, n);
   }
   Node query(int ql, int qr, int x, int lx, int rx) {
-    if (ql >= rx || qr <= lx) return Node();
-    if (ql <= lx && rx <= qr) return nodes[x];
-    auto mid = lx + (rx - lx) / 2;
+    if (ql >= rx || qr <= lx) {
+      return Node();
+    }
+    if (ql <= lx && rx <= qr) {
+      return nodes[x];
+    }
+    const auto mid = lx + (rx - lx) / 2;
     return Node::unite(query(ql, qr, 2 * x + 1, lx, mid), query(ql, qr, 2 * x + 2, mid, rx));
   }
-  Node query(int ql, int qr) { return query(ql, qr, 0, 0, n); }
-  Node query(int pos) { return query(pos, pos + 1, 0, 0, n); }
+  Node query(int ql, int qr) {
+    return query(ql, qr, 0, 0, n);
+  }
+  Node query(int pos) {
+    return query(pos, pos + 1, 0, 0, n);
+  }
   template <typename F> int findFirst(int ql, int qr, F&& pred, int x, int lx, int rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) return -1;
-    if (rx - lx == 1) return lx;
-    auto mid = lx + (rx - lx) / 2;
+    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
+      return -1;
+    }
+    if (rx - lx == 1) {
+      return lx;
+    }
+    const auto mid = lx + (rx - lx) / 2;
     auto res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
     if (res == -1) {
       res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
@@ -201,9 +230,13 @@ template <typename Node> struct SegmentTree {
     return findFirst(ql, qr, std::forward<F>(pred), 0, 0, n);
   }
   template <typename F> int findLast(int ql, int qr, F&& pred, int x, int lx, int rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) return -1;
-    if (rx - lx == 1) return lx;
-    auto mid = lx + (rx - lx) / 2;
+    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
+      return -1;
+    }
+    if (rx - lx == 1) {
+      return lx;
+    }
+    const auto mid = lx + (rx - lx) / 2;
     auto res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
     if (res == -1) res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
     return res;
@@ -214,9 +247,9 @@ template <typename Node> struct SegmentTree {
 };
 
 struct Node {
-  int64_t val = 0;
-  static Node unite(Node a, Node b) {
-    return Node{std::max(a.val, b.val)};
+  int val = 0;
+  static Node unite(const Node& a, const Node& b) {
+    return {std::max(a.val, b.val)};
   }
 };
 
@@ -225,13 +258,13 @@ template <typename Node> struct BottomUpSegmentTree {
   int n = 0;
   std::vector<Node> nodes;
 
+  BottomUpSegmentTree() = default;
   explicit BottomUpSegmentTree(int n_) { init(std::vector(n_, Node())); }
-  BottomUpSegmentTree(int n_, Node node) { init(std::vector(n_, node)); }
-  template <typename T> explicit BottomUpSegmentTree(std::vector<T> init_) { init(init_); }
+  BottomUpSegmentTree(int n_, const Node& node) { init(std::vector(n_, node)); }
+  template <typename T> explicit BottomUpSegmentTree(const std::vector<T>& init_) { init(init_); }
 
-  template <typename T> void init(std::vector<T> init_) {
-    n = 1;
-    for (; n < init_.size(); n *= 2) { }
+  template <typename T> void init(const std::vector<T>& init_) {
+    for (n = 1; n < init_.size(); n *= 2) { }
     nodes.assign(2 * n, Node());
     for (int i = 0; i < init_.size(); ++i) {
       nodes[i + n] = Node(init_[i]);
@@ -241,7 +274,7 @@ template <typename Node> struct BottomUpSegmentTree {
     }
   }
 
-  void modify(int pos, Node val) {
+  void modify(int pos, const Node& val) {
     nodes[pos += n] = val;
     for (pos >>= 1; pos >= 1; pos >>= 1) {
       nodes[pos] = Node::unite(nodes[pos << 1], nodes[pos << 1 | 1]);
@@ -250,16 +283,20 @@ template <typename Node> struct BottomUpSegmentTree {
   Node query(int l, int r) {
     auto retL = Node(), retR = Node();
     for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) retL = Node::unite(retL, nodes[l++]);
-      if (r & 1) retR = Node::unite(nodes[--r], retR);
+      if (l & 1) {
+        retL = Node::unite(retL, nodes[l++]);
+      }
+      if (r & 1) {
+        retR = Node::unite(nodes[--r], retR);
+      }
     }
     return Node::unite(retL, retR);
   }
 };
 
 struct Node {
-  int64_t val = 0;
-  static Node unite(Node a, Node b) {
-    return Node{a.val + b.val};
+  int val = 0;
+  static Node unite(const Node& a, const Node& b) {
+    return {std::max(a.val, b.val)};
   }
 };
