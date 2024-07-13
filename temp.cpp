84118ee571b2f83717adc8b66030c5e8fccec7d3
aminjonshermatov <aminjonshermatov@gmail.com>
2024-07-13 17:37:33 +0300

    temp.cpp | updated

--- temp.cpp
+++ temp.cpp
@@ -6,9 +6,9 @@
 #define dbg(...) 42
 #endif
 
-constexpr void scan(auto&&... args) noexcept { (std::cin >> ... >> args); }
-constexpr void print(auto&&... args) noexcept { ((std::cout << args << ' '), ...); }
-constexpr void println(auto&&... args) noexcept { print(std::forward<decltype(args)>(args)...); std::cout << '\n'; }
+constexpr void scan(auto&& ... args) noexcept { (std::cin >> ... >> args); }
+constexpr void print(auto&& ... args) noexcept { ((std::cout << args << ' '), ...); }
+constexpr void println(auto&& ... args) noexcept { print(std::forward<decltype(args)>(args)...); std::cout << '\n'; }
 
 void solve() {
 }
@@ -16,6 +16,10 @@ void solve() {
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
+
+#ifdef LOCAL
+  freopen("../../input.txt", "r", stdin);
+#endif
 
   solve();
 }
