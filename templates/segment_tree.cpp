0ceaa944a52743a6d72e03331f28934af9d919de
aminjonshermatov <aminjonshermatov@gmail.com>
2024-11-16 01:09:36 +0300

    templates/segment tree

--- templates/segment_tree.cpp
+++ templates/segment_tree.cpp
@@ -5,376 +5,399 @@
 
 using namespace std;
 
-template <typename Node, typename Tag>
-struct LazySegmentTree {
-  std::size_t n{};
-  std::vector<Node> nodes;
-  std::vector<Tag> tags;
+template <typename Info, typename Tag>
+class LazySegmentTree {
+ public:
+  static constexpr auto kNpos = static_cast<size_t>(-1);
 
-  static constexpr auto npos = static_cast<size_t>(-1);
-
+ public:
   LazySegmentTree() = default;
 
-  explicit LazySegmentTree(std::size_t n_) { init(std::vector(n_, Node())); }
-
-  LazySegmentTree(std::size_t n_, Node&& node) {
-    init(std::vector(n_, std::forward<Node>(node)));
+  explicit LazySegmentTree(std::size_t n) {
+    Init(std::vector(n, Info{}));
+  }
+  LazySegmentTree(std::size_t n, Info info) {
+    Init(std::vector(n, info));
+  }
+  template <typename T>
+  explicit LazySegmentTree(const std::vector<T>& init) {
+    Init(init);
   }
 
   template <typename T>
-  explicit LazySegmentTree(const std::vector<T>& init_) { init(init_); }
+  void Init(const std::vector<T>& init) {
+    assert(!init.empty());
+    N_ = 1u << std::__lg(2 * init.size() - 1);
+    Infos_.assign(2 * N_ - 1, Info{});
+    Tags_.assign(2 * N_ - 1, Tag{});
 
-  template <typename T>
-  void init(const std::vector<T>& init_) {
-    n = 1u << std::__lg(2 * init_.size() - 1);
-    nodes.assign(2 * n - 1, Node());
-    tags.assign(2 * n - 1, Tag());
     auto build = [&](auto&& self, std::size_t x, std::size_t lx, std::size_t rx) -> void {
       if (rx - lx == 1u) {
-        if (lx < init_.size()) {
-          nodes[x] = Node{init_[lx]};
+        if (lx < init.size()) {
+          Infos_[x] = {init[lx]};
         }
         return;
       }
       const auto mid = lx + (rx - lx) / 2;
       self(self, 2 * x + 1, lx, mid);
       self(self, 2 * x + 2, mid, rx);
-      nodes[x] = Node::unite(std::forward<Node>(nodes[2 * x + 1]),
-                             std::forward<Node>(nodes[2 * x + 2]));
+      Infos_[x] = Infos_[2 * x + 1] + Infos_[2 * x + 2];
     };
-    build(build, 0u, 0u, n);
+    build(build, 0u, 0u, N_);
   }
 
-  void apply(std::size_t x, Tag&& tag) {
-    nodes[x].apply(std::forward<Tag>(tag));
-    tags[x].apply(std::forward<Tag>(tag));
+  void Modify(std::size_t pos, Info info) {
+    ModifyImpl(pos, info, 0u, 0u, N_);
+  }
+  void Modify(std::size_t ql, std::size_t qr, Tag tag) {
+    ModifyImpl(ql, qr, tag, 0u, 0u, N_);
   }
 
-  void push(std::size_t x) {
-    apply(2 * x + 1, std::forward<Tag>(tags[x]));
-    apply(2 * x + 2, std::forward<Tag>(tags[x]));
-    tags[x] = Tag();
+  Info Query(std::size_t ql, std::size_t qr) {
+    return QueryImpl(ql, qr, 0u, 0u, N_);
+  }
+  Info Query(std::size_t pos) {
+    return QueryImpl(pos, pos + 1, 0u, 0u, N_);
   }
 
-  void pull(std::size_t x) {
-    nodes[x] = Node::unite(std::forward<Node>(nodes[2 * x + 1]),
-                           std::forward<Node>(nodes[2 * x + 2]));
+  template <typename Functor>
+  std::size_t FindFirst(std::size_t ql, std::size_t qr, Functor&& functor) {
+    return FindFirstImpl(ql, qr, std::move(functor), 0u, 0u, N_);
+  }
+  template <typename Functor>
+  std::size_t FindLast(std::size_t ql, std::size_t qr, Functor&& functor) {
+    return FindLastImpl(ql, qr, std::move(functor), 0u, 0u, N_);
   }
 
-  void modify(std::size_t pos, Node&& node,
-              std::size_t x, std::size_t lx, std::size_t rx) {
+ private:
+  void Apply(std::size_t x, Tag tag) {
+    Infos_[x].Apply(tag);
+    Infos_[x].Apply(tag);
+  }
+
+  void Push(std::size_t x) {
+    Apply(2 * x + 1, Tags_[x]);
+    Apply(2 * x + 2, Tags_[x]);
+    Tags_[x] = Tag{};
+  }
+
+  void Pull(std::size_t x) {
+    Infos_[x] = Infos_[2 * x + 1] + Infos_[2 * x + 2];
+  }
+
+  void ModifyImpl(std::size_t pos, Info info, std::size_t x, std::size_t lx, std::size_t rx) {
     if (rx - lx == 1u) {
-      nodes[x] = node;
+      Infos_[x] = info;
       return;
     }
-    push(x);
+    Push(x);
     const auto mid = lx + (rx - lx) / 2;
     if (pos < mid) {
-      modify(pos, std::forward<Node>(node), 2 * x + 1, lx, mid);
+      ModifyImpl(pos, info, 2 * x + 1, lx, mid);
     } else {
-      modify(pos, std::forward<Node>(node), 2 * x + 2, mid, rx);
+      ModifyImpl(pos, info, 2 * x + 2, mid, rx);
     }
-    pull(x);
+    Pull(x);
   }
 
-  void modify(std::size_t pos, Node&& node) {
-    modify(pos, std::forward<Node>(node), 0u, 0u, n);
-  }
-
-  void modify(std::size_t ql, std::size_t qr, Tag&& tag,
-              std::size_t x, std::size_t lx, std::size_t rx) {
+  void ModifyImpl(std::size_t ql, std::size_t qr, Tag tag, std::size_t x, std::size_t lx, std::size_t rx) {
     if (ql >= rx || qr <= lx) {
       return;
     }
     if (ql <= lx && rx <= qr) {
-      apply(x, std::forward<Tag>(tag));
+      Apply(x, tag);
       return;
     }
-    push(x);
+    Push(x);
     const auto mid = lx + (rx - lx) / 2;
-    modify(ql, qr, std::forward<Tag>(tag), 2 * x + 1, lx, mid);
-    modify(ql, qr, std::forward<Tag>(tag), 2 * x + 2, mid, rx);
-    pull(x);
+    ModifyImpl(ql, qr, tag, 2 * x + 1, lx, mid);
+    ModifyImpl(ql, qr, tag, 2 * x + 2, mid, rx);
+    Pull(x);
   }
 
-  void modify(std::size_t ql, std::size_t qr, Tag&& tag) {
-    modify(ql, qr, std::forward<Tag>(tag), 0u, 0u, n);
-  }
-
-  Node query(std::size_t ql, std::size_t qr,
-             std::size_t x, std::size_t lx, std::size_t rx) {
+  Info QueryImpl(std::size_t ql, std::size_t qr, std::size_t x, std::size_t lx, std::size_t rx) {
     if (ql >= rx || qr <= lx) {
-      return Node();
+      return {};
     }
     if (ql <= lx && rx <= qr) {
-      return nodes[x];
+      return Infos_[x];
     }
-    push(x);
+    Push(x);
     const auto mid = lx + (rx - lx) / 2;
-    return Node::unite(std::forward<Node>(query(ql, qr, 2 * x + 1, lx, mid)),
-                       std::forward<Node>(query(ql, qr, 2 * x + 2, mid, rx)));
+    return QueryImpl(ql, qr, 2 * x + 1, lx, mid) + QueryImpl(ql, qr, 2 * x + 2, mid, rx);
   }
 
-  Node query(std::size_t ql, std::size_t qr) {
-    return query(ql, qr, 0u, 0u, n);
-  }
-
-  Node query(std::size_t pos) {
-    return query(pos, pos + 1, 0u, 0u, n);
-  }
-
-  template <typename F>
-  std::size_t findFirst(std::size_t ql, std::size_t qr, F&& pred,
-                        std::size_t x, std::size_t lx, std::size_t rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
-      return npos;
+  template <typename Functor>
+  std::size_t FindFirstImpl(
+    std::size_t ql, std::size_t qr, Functor&& functor,
+    std::size_t x, std::size_t lx, std::size_t rx
+  ) {
+    if (lx >= qr || rx <= ql || !functor(Infos_[x])) {
+      return kNpos;
     }
     if (rx - lx == 1u) {
       return lx;
     }
-    push(x);
+    Push(x);
     const auto mid = lx + (rx - lx) / 2;
-    auto res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
-    if (res == npos) {
-      res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
+
+    auto res = FindFirstImpl(ql, qr, std::move(functor), 2 * x + 1, lx, mid);
+    if (res == kNpos) {
+      res = FindFirstImpl(ql, qr, std::move(functor), 2 * x + 2, mid, rx);
     }
     return res;
   }
-
-  template <typename F>
-  std::size_t findFirst(std::size_t ql, std::size_t qr, F&& pred) {
-    return findFirst(ql, qr, std::forward<F>(pred), 0u, 0u, n);
-  }
-
-  template <typename F>
-  std::size_t findLast(std::size_t ql, std::size_t qr, F&& pred,
-                       std::size_t x, std::size_t lx, std::size_t rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
-      return npos;
+  template <typename Functor>
+  std::size_t FindLastImpl(
+    std::size_t ql, std::size_t qr, Functor&& functor,
+    std::size_t x, std::size_t lx, std::size_t rx
+  ) {
+    if (lx >= qr || rx <= ql || !functor(Infos_[x])) {
+      return kNpos;
     }
     if (rx - lx == 1u) {
       return lx;
     }
-    push(x);
+    Push(x);
     const auto mid = lx + (rx - lx) / 2;
-    auto res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
-    if (res == npos) {
-      res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
+
+    auto res = FindLastImpl(ql, qr, std::move(functor), 2 * x + 2, mid, rx);
+    if (res == kNpos) {
+      res = FindLastImpl(ql, qr, std::move(functor), 2 * x + 1, lx, mid);
     }
     return res;
   }
 
-  template <typename F>
-  std::size_t findLast(std::size_t ql, std::size_t qr, F&& pred) {
-    return findLast(ql, qr, std::forward<F>(pred), 0u, 0u, n);
-  }
+ private:
+  std::size_t N_{0};
+  std::vector<Info> Infos_;
+  std::vector<Tag> Tags_;
 };
 
-struct Tag {
-  int val = 0;
-  void apply(auto&& tag) {
-    val += tag.val;
+class Tag {
+ public:
+  void Apply(auto&& tag) {
+    Val += tag.Val;
   }
+
+ public:
+  int Val = 0;
 };
 
-struct Node {
-  int min = std::numeric_limits<int>::max() / 2;
-  void apply(auto&& tag) {
-    min += tag.val;
+class Info {
+ public:
+  void Apply(const Tag& tag) {
+    Min += tag.Val;
   }
-  static Node unite(auto&& a, auto&& b) {
-    return {std::min(a.min, b.min)};
-  }
+
+ public:
+  int Min = std::numeric_limits<int>::max() / 2;
 };
+Info operator+(const Info& lhs, const Info& rhs) {
+  return {std::min(lhs.Min, rhs.Min)};
+}
 
-template <typename Node>
-struct SegmentTree {
-  std::size_t n{};
-  std::vector<Node> nodes;
 
-  static constexpr auto npos = static_cast<size_t>(-1);
+template <typename Info>
+class SegmentTree {
+ public:
+  static constexpr auto kNpos = static_cast<size_t>(-1);
 
+ public:
   SegmentTree() = default;
 
-  explicit SegmentTree(std::size_t n_) { init(std::vector(n_, Node())); }
-
-  SegmentTree(std::size_t n_, Node&& node) { init(std::vector(n_, std::forward<Node>(node))); }
+  explicit SegmentTree(std::size_t n) {
+    Init(std::vector(n, Info{}));
+  }
+  SegmentTree(std::size_t n, Info info) {
+    Init(std::vector(n, info));
+  }
+  template <typename T>
+  explicit SegmentTree(const std::vector<T>& init) {
+    Init(init);
+  }
 
   template <typename T>
-  explicit SegmentTree(const std::vector<T>& init_) { init(init_); }
+  void Init(const std::vector<T>& init) {
+    assert(!init.empty());
+    N_ = 1u << std::__lg(2 * init.size() - 1);
+    Infos_.assign(2 * N_ - 1, Info{});
 
-  template <typename T>
-  void init(const std::vector<T>& init_) {
-    n = 1u << std::__lg(2 * init_.size() - 1);
-    nodes.assign(2 * n - 1, Node());
     auto build = [&](auto&& self, std::size_t x, std::size_t lx, std::size_t rx) -> void {
       if (rx - lx == 1u) {
-        if (lx < init_.size()) {
-          nodes[x] = Node{init_[lx]};
+        if (lx < init.size()) {
+          Infos_[x] = {init[lx]};
         }
         return;
       }
       const auto mid = lx + (rx - lx) / 2;
       self(self, 2 * x + 1, lx, mid);
       self(self, 2 * x + 2, mid, rx);
-      nodes[x] = Node::unite(std::forward<Node>(nodes[2 * x + 1]),
-                             std::forward<Node>(nodes[2 * x + 2]));
+      Infos_[x] = Infos_[2 * x + 1] + Infos_[2 * x + 2];
     };
-    build(build, 0u, 0u, n);
+    build(build, 0u, 0u, N_);
   }
 
-  void modify(std::size_t pos, Node&& node,
-              std::size_t x, std::size_t lx, std::size_t rx) {
+  void Modify(std::size_t pos, Info info) {
+    ModifyImpl(pos, info, 0u, 0u, N_);
+  }
+
+  Info Query(std::size_t ql, std::size_t qr) {
+    return QueryImpl(ql, qr, 0u, 0u, N_);
+  }
+  Info Query(std::size_t pos) {
+    return QueryImpl(pos, pos + 1, 0u, 0u, N_);
+  }
+
+  template <typename Functor>
+  std::size_t FindFirst(std::size_t ql, std::size_t qr, Functor&& functor) {
+    return FindFirstImpl(ql, qr, std::move(functor), 0u, 0u, N_);
+  }
+  template <typename Functor>
+  std::size_t FindLast(std::size_t ql, std::size_t qr, Functor&& functor) {
+    return FindLastImpl(ql, qr, std::move(functor), 0u, 0u, N_);
+  }
+
+ private:
+  void ModifyImpl(std::size_t pos, Info info, std::size_t x, std::size_t lx, std::size_t rx) {
     if (rx - lx == 1u) {
-      nodes[x] = node;
+      Infos_[x] = info;
       return;
     }
     const auto mid = lx + (rx - lx) / 2;
     if (pos < mid) {
-      modify(pos, std::forward<Node>(node), 2 * x + 1, lx, mid);
+      ModifyImpl(pos, info, 2 * x + 1, lx, mid);
     } else {
-      modify(pos, std::forward<Node>(node), 2 * x + 2, mid, rx);
+      ModifyImpl(pos, info, 2 * x + 2, mid, rx);
     }
-    nodes[x] = Node::unite(std::forward<Node>(nodes[2 * x + 1]),
-                           std::forward<Node>(nodes[2 * x + 2]));
+    Infos_[x] = Infos_[2 * x + 1] + Infos_[2 * x + 2];
   }
 
-  void modify(std::size_t pos, Node&& node) {
-    modify(pos, std::forward<Node>(node), 0u, 0u, n);
-  }
-
-  Node query(std::size_t ql, std::size_t qr,
-             std::size_t x, std::size_t lx, std::size_t rx) {
+  Info QueryImpl(std::size_t ql, std::size_t qr, std::size_t x, std::size_t lx, std::size_t rx) {
     if (ql >= rx || qr <= lx) {
-      return Node();
+      return {};
     }
     if (ql <= lx && rx <= qr) {
-      return nodes[x];
+      return Infos_[x];
     }
     const auto mid = lx + (rx - lx) / 2;
-    return Node::unite(std::forward<Node>(query(ql, qr, 2 * x + 1, lx, mid)),
-                       std::forward<Node>(query(ql, qr, 2 * x + 2, mid, rx)));
+    return QueryImpl(ql, qr, 2 * x + 1, lx, mid) + QueryImpl(ql, qr, 2 * x + 2, mid, rx);
   }
 
-  Node query(std::size_t ql, std::size_t qr) {
-    return query(ql, qr, 0u, 0u, n);
-  }
-
-  Node query(std::size_t pos) {
-    return query(pos, pos + 1, 0u, 0u, n);
-  }
-
-  template <typename F>
-  std::size_t findFirst(std::size_t ql, std::size_t qr, F&& pred,
-                        std::size_t x, std::size_t lx, std::size_t rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
-      return npos;
+  template <typename Functor>
+  std::size_t FindFirstImpl(
+    std::size_t ql, std::size_t qr, Functor&& functor,
+    std::size_t x, std::size_t lx, std::size_t rx
+  ) {
+    if (lx >= qr || rx <= ql || !functor(Infos_[x])) {
+      return kNpos;
     }
     if (rx - lx == 1u) {
       return lx;
     }
     const auto mid = lx + (rx - lx) / 2;
-    auto res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
-    if (res == npos) {
-      res = findFirst(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
+
+    auto res = FindFirstImpl(ql, qr, std::move(functor), 2 * x + 1, lx, mid);
+    if (res == kNpos) {
+      res = FindFirstImpl(ql, qr, std::move(functor), 2 * x + 2, mid, rx);
     }
     return res;
   }
-
-  template <typename F>
-  std::size_t findFirst(std::size_t ql, std::size_t qr, F&& pred) {
-    return findFirst(ql, qr, std::forward<F>(pred), 0u, 0u, n);
-  }
-
-  template <typename F>
-  std::size_t findLast(std::size_t ql, std::size_t qr, F&& pred,
-                       std::size_t x, std::size_t lx, std::size_t rx) {
-    if (lx >= qr || rx <= ql || !pred(nodes[x])) {
-      return npos;
+  template <typename Functor>
+  std::size_t FindLastImpl(
+    std::size_t ql, std::size_t qr, Functor&& functor,
+    std::size_t x, std::size_t lx, std::size_t rx
+  ) {
+    if (lx >= qr || rx <= ql || !functor(Infos_[x])) {
+      return kNpos;
     }
     if (rx - lx == 1u) {
       return lx;
     }
     const auto mid = lx + (rx - lx) / 2;
-    auto res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 2, mid, rx);
-    if (res == npos) {
-      res = findLast(ql, qr, std::forward<F>(pred), 2 * x + 1, lx, mid);
+
+    auto res = FindLastImpl(ql, qr, std::move(functor), 2 * x + 2, mid, rx);
+    if (res == kNpos) {
+      res = FindLastImpl(ql, qr, std::move(functor), 2 * x + 1, lx, mid);
     }
     return res;
   }
 
-  template <typename F>
-  std::size_t findLast(std::size_t ql, std::size_t qr, F&& pred) {
-    return findLast(ql, qr, std::forward<F>(pred), 0u, 0u, n);
-  }
+ private:
+  std::size_t N_{0};
+  std::vector<Info> Infos_;
 };
 
-struct Node {
-  int val = 0;
-  static Node unite(auto&& a, auto&& b) {
-    return {std::max(a.val, b.val)};
-  }
+class Info {
+ public:
+  int Val = 0;
 };
+Info operator+(const Info& lhs, const Info& rhs) {
+  return {std::max(lhs.Val, rhs.Val)};
+}
 
 
-template <typename Node>
-struct BottomUpSegmentTree {
-  std::size_t n{};
-  std::vector<Node> nodes;
-
+template <typename Info>
+class BottomUpSegmentTree {
+ public:
   BottomUpSegmentTree() = default;
 
-  explicit BottomUpSegmentTree(std::size_t n_) { init(std::vector(n_, Node())); }
-
-  BottomUpSegmentTree(std::size_t n_, Node&& node) {
-    init(std::vector(n_, std::forward<Node>(node)));
+  explicit BottomUpSegmentTree(std::size_t n) {
+    Init(std::vector(n, Info{}));
+  }
+  BottomUpSegmentTree(std::size_t n, Info&& info) {
+    Init(std::vector(n, std::forward<Info>(info)));
+  }
+  template <typename T>
+  explicit BottomUpSegmentTree(const std::vector<T>& init) {
+    Init(init);
   }
 
   template <typename T>
-  explicit BottomUpSegmentTree(const std::vector<T>& init_) { init(init_); }
-
-  template <typename T>
-  void init(const std::vector<T>& init_) {
-    n = 1u << std::__lg(2 * init_.size() - 1);
-    nodes.assign(2 * n, Node());
-    for (std::size_t i{}; i < init_.size(); ++i) {
-      nodes[i + n] = Node{init_[i]};
+  void Init(const std::vector<T>& init) {
+    N_ = 1u << std::__lg(2 * init.size() - 1);
+    Infos_.assign(2 * N_, Info{});
+    for (std::size_t i{0}; i < init.size(); ++i) {
+      Infos_[i + N_] = Info{init[i]};
     }
-    for (std::size_t i = n - 1; i >= 1u; --i) {
-      nodes[i] = Node::unite(std::forward<Node>(nodes[i << 1]),
-                             std::forward<Node>(nodes[i << 1 | 1]));
+    for (std::size_t i = N_ - 1; i >= 1u; --i) {
+      Infos_[i] = Infos_[i << 1] + Infos_[i << 1 | 1];
     }
   }
 
-  void modify(std::size_t pos, Node&& val) {
-    nodes[pos += n] = val;
+  void Modify(std::size_t pos, Info&& val) {
+    Infos_[pos += N_] = val;
     for (pos >>= 1; pos >= 1; pos >>= 1) {
-      nodes[pos] = Node::unite(std::forward<Node>(nodes[pos << 1]),
-                               std::forward<Node>(nodes[pos << 1 | 1]));
+      Infos_[pos] = Infos_[pos << 1] + Infos_[pos << 1 | 1];
     }
   }
 
-  Node query(std::size_t l, std::size_t r) {
-    auto retL = Node(), retR = Node();
-    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+  Info Query(std::size_t l, std::size_t r) {
+    auto retL = Info{}, retR = Info{};
+    for (l += N_, r += N_; l < r; l >>= 1, r >>= 1) {
       if (l % 2 == 1u) {
-        retL = Node::unite(retL, nodes[l++]);
+        retL = retL + Infos_[l++];
       }
       if (r % 2 == 1u) {
-        retR = Node::unite(nodes[--r], retR);
+        retR = Infos_[--r] + retR;
       }
     }
-    return Node::unite(std::forward<Node>(retL), std::forward<Node>(retR));
+    return retL + retR;
   }
 
-  Node query(std::size_t pos) {
-    return query(pos, pos + 1);
+  Info Query(std::size_t pos) {
+    return Query(pos, pos + 1);
   }
+
+ private:
+  std::size_t N_{};
+  std::vector<Info> Infos_;
 };
 
-struct Node {
-  int val = 0;
-  static Node unite(const Node& a, const Node& b) {
-    return {std::max(a.val, b.val)};
-  }
+class Info {
+ public:
+  int Val = 0;
 };
+Info operator+(const Info& lhs, const Info& rhs) {
+  return {std::max(lhs.Val, rhs.Val)};
+}
