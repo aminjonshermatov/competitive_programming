b07abd989df2bfaa3177f8f16d713c3a40fb3528
aminjonshermatov <aminjonshermatov@gmail.com>
2024-11-17 15:27:29 +0300

    templates/sq_matrix

--- templates/square_matrix.cpp
+++ templates/square_matrix.cpp
@@ -3,23 +3,25 @@
 //
 #include <bits/stdc++.h>
 
-template <typename T, std::size_t N> class SquareMatrix {
-  std::array<T, N * N> data{};
-public:
+template <typename T, std::size_t N>
+class SquareMatrix {
+ public:
   constexpr static SquareMatrix I() {
     SquareMatrix<T, N> ret{};
     for (std::size_t i = 0u; i < N; ++i) {
-      ret(i, i) = T(1);
+      ret(i, i) = T{1};
     }
     return ret;
   }
+
   constexpr T& operator()(int i, int j) noexcept {
-    return data[i * N + j];
+    return Data_[i * N + j];
   }
   [[nodiscard]] constexpr T operator()(int i, int j) const noexcept {
-    return data[i * N + j];
+    return Data_[i * N + j];
   }
-  constexpr SquareMatrix& operator*=(const SquareMatrix& other) noexcept {
+
+  constexpr SquareMatrix operator*(const SquareMatrix& other) noexcept {
     SquareMatrix mat{};
     for (std::size_t i = 0u; i < N; ++i) {
       for (std::size_t j = 0u; j < N; ++j) {
@@ -28,24 +30,31 @@ public:
         }
       }
     }
-    return *this = mat;
+    return mat;
   }
-  constexpr SquareMatrix& pow(uint64_t n) noexcept {
+
+  constexpr SquareMatrix Pow(uint64_t n) const noexcept {
     auto ret = SquareMatrix::I();
-    for (; n > 0; n >>= 1, *this *= *this) {
+    auto cur = *this;
+    for (; n > 0; n >>= 1, cur = cur * cur) {
       if (n % 2 == 1) {
-        ret *= *this;
+        ret = ret * cur;
       }
     }
-    return *this = ret;
+    return ret;
   }
-  template <typename U, std::size_t M> friend std::ostream& operator<<(std::ostream& out, const SquareMatrix<U, M>& mat) {
+
+  template <typename U, std::size_t M>
+  friend std::ostream& operator<<(std::ostream& os, const SquareMatrix<U, M>& mat) {
     for (std::size_t i = 0u; i < M; ++i) {
       for (std::size_t j = 0u; j < M; ++j) {
-        out << mat(i, j) << ' ';
+        os << mat(i, j) << ' ';
       }
-      out << '\n';
+      os << '\n';
     }
-    return out;
+    return os;
   }
+
+ private:
+  std::array<T, N * N> Data_{};
 };
