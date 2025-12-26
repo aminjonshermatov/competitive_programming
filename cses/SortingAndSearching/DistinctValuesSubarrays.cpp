fcdedce3d599bbe227fe11359864b4bb70fbc7f5
aminjonshermatov <aminjonshermatov@gmail.com>
2025-12-26 15:42:34 +0300

    cses

---
+++ cses/SortingAndSearching/DistinctValuesSubarrays.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#ifdef LOCAL
+#include "debug.hpp"
+#else
+#define dbg(...) 42
+#endif
+
+constexpr void Scan(auto&& ... args) noexcept { (std::cin >> ... >> args); }
+constexpr void Print(auto&& ... args) noexcept { ((std::cout << args << ' '), ...); }
+constexpr void Println(auto&& ... args) noexcept { Print(std::forward<decltype(args)>(args)...); std::cout << '\n'; }
+
+void Solve() {
+  int n;
+  Scan(n);
+  std::vector<int> as(n);
+  for (int i = 0; i < n; ++i) {
+    Scan(as[i]);
+  }
+
+  std::map<int, int> prv;
+  auto ret = 0LL;
+  for (int i = 0, j = -1; i < n; ++i) {
+    if (prv.contains(as[i])) {
+      j = std::max(j, prv[as[i]]);
+    }
+    ret += i - j;
+    prv[as[i]] = i;
+  }
+  Println(ret);
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  Solve();
+}
